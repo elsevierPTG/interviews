9bb9a8684518e28d9716bb622c73704c9bb70c50
Jasmine Chacko <chackojasmine@gmail.com>
2016-10-01 22:59:05 -0400

    Jasmine Chacko committed code

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,20 +1,34 @@
 package com.elsevier.education;
 
 /**
-
-TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
-
-*/
+ TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
+ */
 public class Exercise5 {
-	
-	public static class Singleton {
-		public void doSomeThing() {
-			System.out.println("Doing something....");
-	    }
-	}
-		
-	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
-	}
+
+    public static class Singleton {
+        // Private static variable of the Singleton class that is the only instance of the class.
+        private static Singleton instance;
+
+        //private constructor to avoid client applications to use constructor
+        private Singleton(){
+        }
+
+        public void doSomeThing() {
+            System.out.println("Doing something....");
+        }
+
+        //Public static method that returns the instance of the class, this is the global access point
+        //for outer world to get the instance of the Singleton class.
+        public static Singleton getInstance(){
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+    public static void main(String a[]){
+        Singleton.getInstance().doSomeThing();
+    }
 }
