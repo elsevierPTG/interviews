994f1a2c2853cfd0c27c94b7802ebe029b9319cd
yarishabh <yarishabh@gmail.com>
2016-10-27 22:49:44 -0400

    Update Exercise3.java

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -1,43 +1,46 @@
-package com.elsevier.education;
+package com.company;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
 
 /**
-
-can run with gradlew clean build; java -cp build/classes/main com.elsevier.education.Exercise3
-
-TODO We should be able to call people.add() twice but end with only one object in it.
-
-*/
+ * Created by Yash Gupta
+ */
 public class Exercise3 {
 
-	public static void main(String[] args) {
-	
-		Set<Person> people = new HashSet<>();
-	
-		Person p1 = new Person(1);
-		people.add(p1);
-		people.add(p1);
-		
-		System.out.println(people.size());
-		
-	}
-	
-	public static class Person {
-	
-		private static Random generator = new java.util.Random();
-		private Integer id;
-		
-		public Person(int newId) {
-			id = newId;
-		}
-		
-		public int hashCode() {
-			return id * generator.nextInt();
-		}
-		
-		public boolean equals(Object other) {
-			return id.equals(((Person)other).id);
-		}
-	}
+    public static void main(String[] args) {
+
+        Set<Person> people = new HashSet<Person>();
+
+        Person p1 = new Person(1);
+        people.add(p1);
+        people.add(p1);
+
+        System.out.println(people.size());
+
+    }
+
+    public static class Person {
+
+        private static Random generator = new java.util.Random();
+        private Integer id;
+
+        public Person(int newId) {
+            id = newId;
+        }
+
+        public int hashCode() {
+            final int prime = 47;
+            int hashValue = 1;
+            hashValue = prime * hashValue + ((id == null) ? 0 : id.hashCode());
+            return hashValue;
+        }
+
+        public boolean equals(Object other) {
+            return id.equals(((Person)other).id);
+        }
+
+    }
 }
