0844d3c56e1e5cdccd584d2c529b51993a15edb9
VijayaRemala <Vijaya.Remala@cognizant.com>
2016-11-01 16:02:57 +0530

    Updated
    VijayRemala:Vijaya.Remala@cognizant.com

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,13 +8,30 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		
+		Singleton s=null;
+		private Singleton(){
+			
+		}
+		
+		public static Singleton getSingleton(Singleton s){
+			
+			if(s==null){
+				
+			s=new Singleton();
+				
+				
+			}		
+			
+		return s;	
+		}
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st =Singleton.getSingleton();
 		st.doSomeThing();
 	}
 }
