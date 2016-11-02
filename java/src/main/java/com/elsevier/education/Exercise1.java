f8ff5cecd1a318b7b726b5ab1798d7a4af7ced8c
bhupalbekkam <bhupal.bekkam@gmail.com>
2016-11-02 19:50:26 -0400

    Solutions
    Name: Bhupal Reddy
    Email: bhupal.bekkam@gmail.com

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -7,12 +7,14 @@ TODO: Make this class immutable.
 */
 public class Exercise1 {
 
-	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
+	//Class which is marked as final cannot be changed / inherited thus makes it an immutable class
+	public final class Person {
+
+		//making all the  variables as final will not let other to mutate
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
+
 		public Person() {
 		}
 
@@ -22,14 +24,14 @@ public class Exercise1 {
 		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
 		}
-		
+
 		public String getFirstName() {
 			return firstName;
 		}
 		public void setFirstName(String newName) {
 			firstName = newName;
 		}
-		
+
 		public String getLastName() {
 			return lastName;
 		}
--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -12,30 +12,32 @@ TODO We should be able to call people.add() twice but end with only one object i
 public class Exercise3 {
 
 	public static void main(String[] args) {
-	
+
 		Set<Person> people = new HashSet<>();
-	
+
 		Person p1 = new Person(1);
 		people.add(p1);
 		people.add(p1);
-		
+
 		System.out.println(people.size());
-		
+
 	}
-	
+
 	public static class Person {
-	
+
 		private static Random generator = new java.util.Random();
 		private Integer id;
-		
+
 		public Person(int newId) {
 			id = newId;
 		}
-		
+
+
+		//returning id as the hashcode will make it add the same object twice and since it is same, there will be only one object in the //set.
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id ;
 		}
-		
+
 		public boolean equals(Object other) {
 			return id.equals(((Person)other).id);
 		}
