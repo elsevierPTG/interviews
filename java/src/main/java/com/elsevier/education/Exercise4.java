c835b3a66b4c460e965fed48c459fcce2ecb80d4
mtrdinesh <mtrdinesh@gmail.com>
2016-11-02 23:19:42 -0500

    Update Exercise4.java

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,15 +1,17 @@
 package com.elsevier.education;
 
 /**
-
+ *  * @author Dineshkumar Periyasamy mtrdinesh@gmail.com
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+No Counter is not thread safe, we need to make the variable count as volatile
 
+Volatile will directly write into memory and behave as Synchronized block enclosure
 */
 public class Exercise4 {
 
 	public static class Counter {
 		
-		private int count = 0;
+		private volatile int count = 0;
 		
 		public int increment() {
 			return ++count;
