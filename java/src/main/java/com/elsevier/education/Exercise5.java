9d83a6e0eacc111b0d8fca799a76b36dbf6577bf
Partha <maitra.partha@gmail.com>
2016-11-06 13:56:55 -0500

    Code Submission for interview. -Name : Partha Maitra -email: partha.maitra@cognizant.com

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,5 +1,6 @@
 package com.elsevier.education;
 
+
 /**
 
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
@@ -8,13 +9,29 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		private static volatile Singleton instance;
+		private Singleton(){
+			//creating the private constructor
+		}
+		public static Singleton getSingletonInstance(){
+			if(null == instance){
+				synchronized (Singleton.class) {
+					//double locking -- checking the object for null again before sending back
+					if(null == instance)
+						instance = new Singleton();
+				}
+			}
+			return instance;
+		}
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		//need to access the object using the factory method 
+		//used for returning the singleton object
+		Singleton st = Singleton.getSingletonInstance();
 		st.doSomeThing();
 	}
 }
