105939b0513f99b2a7002410f4efcdb5beb59ed3
rkt1661 <rkt1661@gmail.com>
2016-11-09 13:48:59 -0500

    ramakrishna:rkt1661@gmail.com

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,29 +1,108 @@
 package com.elsevier.education;
 
 /**
+to update to Java dependency injection, here used interface.
 
-TODO refactor the Car to use dependency injection of the engine
-TODO allow use of either a gas engine or electric engine. 
-TODO make sure we have no-op implementations of both engines.
+Dependency Injection main use is reduce coupling in our application and to make it more flexible and easier to test.
+
+Objects don't have hard coded dependencies( creating using new operator). If you need to change the implementation of a dependency, just we need to Inject a different type of Object.
 
 */
 public class Exercise2 {
 
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
-	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
+
+public static interface Engine{
+
+public void spinWheels();
+
+}
+
+
+
+public static class Car {
+
+
+private Engine engine = null;
+
+
+public Car(Engine engine) {
+
+this.engine=engine;
+
+}
+
+
+public void moveForward() {
+
+this.engine.spinWheels();
+
+}
+
+}
+
+
+public static class GasEngine implements Engine{
+
+@Override
+
+public void spinWheels() {
+
+// no-op for now
+
+}
+
+}
+
+
+public static class ElectricEngine implements Engine{
+
+@Override
+
+public void spinWheels() {
+
+// no-op for now
+
+}
+
+}
+
+
+public static interface EngineInjector{
+
+
+public Car getCar();
+
+
+}
+
+
+public static class GasEngineInjector implements EngineInjector{
+
+
+@Override
+
+public Car getCar(){
+
+return new Car(new GasEngine());
+
+}
+
+
+
+}
+
+public static class ElectricEngineInjector implements EngineInjector{
+
+@Override
+
+public Car getCar(){
+
+return new Car(new ElectricEngine());
+
+}
+
+}
+
+
+
 }
