a568447b9a30f236ada31879d443d3e8b0ee7d59
santhoshramr <santhoshram.rathinam@gmail.com>
2016-11-11 15:07:33 +0530

    Update Exercise2.java
    Removed the factory pattern and added Dependency Injection to create object, allowed to use either Gas or Electric engine

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -10,18 +10,27 @@ TODO make sure we have no-op implementations of both engines.
 public class Exercise2 {
 
 	public static class Car {
+	//removed the factory pattern and added Dependency Injection to create objects	
+		private GasEngine engine;
+		private ElectricEngine electricEngine;
 		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
+		public Car(GasEngine engine, ElectricEngine electricEngine;) {
+		this.engine = engine;
+			this.electricEngine = electricEngine;
 		}
 		
 		public void moveForward() {
 			engine.spinWheels();
+			electricEngine.spinWheels();
 		}
 	}
 	
-	public static class GasEngine {
+	public static class GasEngine extends Car {
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
+	public static class ElectricEngine extends car{
 		public void spinWheels() {
 			// no-op for now
 		}
