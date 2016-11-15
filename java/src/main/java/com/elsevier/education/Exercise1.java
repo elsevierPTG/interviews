bae2a30047b2b14c6b8d9e96de10dbe3255aa5ef
khadarvali <khadarvalipothuganti@gmail.com>
2016-11-15 17:42:15 +0530

    Updated Exercise1.java
    Modfied class so as to make this class immutable.

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,5 +1,7 @@
 package com.elsevier.education;
 
+import java.util.HashSet
+import java.util.Set
 /**
 
 TODO: Make this class immutable.
@@ -7,34 +9,53 @@ TODO: Make this class immutable.
 */
 public class Exercise1 {
 
-	public static class Person {
+	//Make this class final so as to make sure this class is not inherited by any other class. This way we can make sure 
+	//this class object is not modified.
+	public final static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		//make all the variables final so that these variables are not modified else where.
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
-		}
+		public Person(Set<String> phoneNumbers, String firstName, String lastName) {
+			this.phoneNumbers = phoneNumbers;
+			this.firstName = firstName;
+			this.lastName = lastName;
+ 		}
 
+	//Removed all the setter methods inside so that the variables are not modifed anywhere.
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
-		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
+		}		
 		
 		public String getFirstName() {
 			return firstName;
-		}
-		public void setFirstName(String newName) {
-			firstName = newName;
 		}
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		
 	}
+	
+	public static void main(String[] args) {
+	//Create HashSet Object
+		Set<String> setData = new HashSet<>();		
+		setData.add("data element");
+		Person personObject = new Person(setData, "firstname", "lastname");
+		//Try to add data to the immutable object personObject as shown below, It will throw set is non editable exception.
+		try {
+			personObject.getPhoneNumbers().add("data element second");
+		} catch Exception e) {
+			System.out.println("Set is non editable);
+ 		}
+		//Try to remove the existing datqa from the set as shown below, It will throw set is non editable exception.
+		try {
+			personObject.getPhoneNumbers().remove("data element");
+		} catch Exception e) {
+			System.out.println("Set is non editable");
+		}
+
+ 	}
 }
