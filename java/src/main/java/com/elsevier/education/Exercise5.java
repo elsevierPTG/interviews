f17d13be999ae7190648fcc1314b798b2d53c706
khadarvali <khadarvalipothuganti@gmail.com>
2016-11-15 18:07:50 +0530

    Update Exercise5.java
    made modifications as per the instructions give.

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -7,14 +7,25 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 */
 public class Exercise5 {
 	
-	public static class Singleton {
+	public final static class Singleton {
+		private static final Singleton INSTANCEOBJECT = new Singleton();
+		//define this method as static so that this can be access outside the class.	
+		public static Singleton getInstanceObject() {
+			return INSTANCEOBJECT;
+		}
+		//make the constructor private so that this class object is not created outside the class.
+		private Singleton() {
+		//Constructor logic goes here.
+		}
+
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
+		//call the static method getInstanceObject() using the classname as it is static.
+		//This returns the object of Singleton class. Using this object call doSomeThing() method.
+		Singleton.getInstanceObject().doSomeThing();
 	}
 }
