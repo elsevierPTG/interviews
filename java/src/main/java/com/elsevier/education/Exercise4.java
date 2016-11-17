adf9decb5cbf2b65e081bf08da74a43da9ff0c35
567721 <utkal.swain@cognizant.com>
2016-11-17 12:40:28 +0530

    Update Exercise4.java
    1-Using volatile keyword with variables also we can make thread safe example private volatile int count;

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -9,6 +9,10 @@ public class Exercise4 {
 
 	public static class Counter {
 		
+		/* is not an atomic operation and can be broken down into read, update and write operation. 
+		if multiple thread call getCount() approximately same time each of these three operation may coincide or overlap
+		with each other */
+	
 		private int count = 0;
 		
 		public int increment() {
@@ -25,3 +29,26 @@ public class Exercise4 {
 
 	}
 }
+
+Solution:
+
+public class Exercise4 {
+
+	public static class Counter {
+		private int count;
+   		 AtomicInteger atomicCount = new AtomicInteger( 0 );
+
+  
+    /*
+     * This method thread-safe now because of locking and synchornization
+     */
+    public synchronized int getCount(){
+        return count++;
+    }
+  
+    /*
+     * This method is thread-safe because count is incremented atomically
+     */
+    public int getCountAtomically(){
+        return atomicCount.incrementAndGet();
+    }
