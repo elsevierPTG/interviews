1fe141c5398b3396418594d9f66f252abae2d3d4
567721 <utkal.swain@cognizant.com>
2016-11-17 12:40:22 +0530

    Update Exercise5.java
    Singleton Class guidelines:
    1-Private constructor to restrict instantiation of the class from other classes.
    2-Private static variable of the same class that is the only instance of the class.
    3-Public static method that returns the instance of the class, this is the global access point for outer world to get the instance of the singleton class.

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,13 +8,23 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		private static Singleton singleton = new Singleton( );
+		
+		/* A private Constructor prevents any other class from instantiating. */
+  		 private Singleton() { }
+		
+		/* Static 'instance' method */
+ 		  public static Singleton getInstance( ) {
+     			 return singleton;
+  			 }
+		
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
