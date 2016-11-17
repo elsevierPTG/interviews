d285a7586836e3f3b4ef530291de27872e6b8997
LakshmiSainath <lakshmidevi.sainathsubramanian@cognizant.com>
2016-11-17 16:15:16 +0530

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -6,35 +6,50 @@ TODO: Make this class immutable.
 
 */
 public class Exercise1 {
-
-	public static class Person {
+	// Marking Person class final so that it cannot be extended by another class
+	public static final class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		// Marking all variables as final so that values once initialized, cannot be set again.
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
-		}
-
+		// Instead of default constructor, constructor with deep copy of all fields is added, so that all fields within 
+		// immutable class are initialized during construction itself.
+		
+		/*public Person() {
+		}*/
+		
+		
+		public Person(Set<String> phoneNums, String firstName, String lastName){			
+			this.phoneNumbers = new HashSet<String>();
+			for(String phoneNum : phoneNums){
+				this.phoneNumbers.add(phoneNum);		
+			}
+			this.firstName =  firstName;
+			this.lastName =  lastName;
+		}		
+		
+		// All setter methods are removed. Final variables once constructed during construction, cannot be changed again.
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
-		}
+		}*/
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
+		/*public void setFirstName(String newName) {
 			firstName = newName;
-		}
+		}*/
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
+		/*public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
