7df0d0e3aa83e55fc005724f4b1c0224ccd08174
boopathyc <boopathyc79@gmail.com>
2016-11-21 14:31:57 +0530

    Update Exercise1.java
    changed to immutable class

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,40 +1,35 @@
 package com.elsevier.education;
 
-/**
+public final class Exercise1 {
 
-TODO: Make this class immutable.
-
-*/
-public class Exercise1 {
-
-	public static class Person {
+	public final static class Person {
+		/*
+		 * The class is changed to final,so that it cannot be extended.
+		 * instance variables are changed to final.
+		 * set methods are removed, so that values cannot be modified after the instance is created.
+		 */
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		public Person(Set<String> phoneNumbers,String firstName,String lastName) {
+			this.phoneNumbers=phoneNumbers;
+			this.firstName=firstName;
+			this.lastName=lastName;			
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
+			
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
+				
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		
 	}
 }
