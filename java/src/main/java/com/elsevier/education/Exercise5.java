74b0f42ada556370fd55f10212d87a2c75702a7a
rssathish <rssathishmca@gmail.com>
2016-11-30 13:00:54 +0530

    Class has been made as Singleton

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,13 +8,28 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+               
+         private static Singleton singletonObject; 
+
+         private Singleton(){      // created private constructor in order to create object from outside.
+	 }
+
+	public static Singleton getInstance() { // This method will create the new object if not exist already.
+		if(singleton != null){ 
+		       return singleton;
+		}
+		else{
+		       return new Singleton();
+		}
+
+	    }
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getInstance(); // calling the getInstance method rather than creating every time.
 		st.doSomeThing();
 	}
 }
