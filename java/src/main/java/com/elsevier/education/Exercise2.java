a20a02bd506402781aa4e49d559d821a6ca432d5
HarishaAkasapu <harisha.akasapu@gmail.com>
2016-12-08 14:58:14 -0500

    Harisha: Refactored code with DI

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,29 +1,40 @@
 package com.elsevier.education;
 
 /**
-
-TODO refactor the Car to use dependency injection of the engine
-TODO allow use of either a gas engine or electric engine. 
-TODO make sure we have no-op implementations of both engines.
-
-*/
+ TODO refactor the Car to use dependency injection of the engine
+ TODO allow use of either a gas engine or electric engine.
+ TODO make sure we have no-op implementations of both engines.
+ */
 public class Exercise2 {
-
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
-	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
+    
+    public static class Car {
+        
+        private Engine engine;
+        
+        //Injecting Engine
+        public Car(Engine engine) {
+            this.engine = engine;
+        }
+        
+        public void moveForward() {
+            engine.spinWheels();
+        }
+    }
+    
+    public static interface Engine {
+        void spinWheels();
+    }
+    
+    public static class GasEngine implements Engine {
+        public void spinWheels() {
+            // no-op for now
+        }
+    }
+    
+    //Created new Electric Engine
+    public static class ElectricEngine implements Engine {
+        public void spinWheels() {
+            // no-op
+        }
+    }
 }
