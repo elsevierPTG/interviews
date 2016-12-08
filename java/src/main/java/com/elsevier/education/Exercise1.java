a50b36f6c36c68ae99feb1a0d15359b7e737cf3d
HarishaAkasapu <harisha.akasapu@gmail.com>
2016-12-08 14:53:33 -0500

    Harisha: Class made immutable

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,40 +1,53 @@
 package com.elsevier.education;
 
+import java.util.Collections;
+import java.util.Set;
+
 /**
-
 TODO: Make this class immutable.
-
 */
 public class Exercise1 {
 
 	public static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		//Private Constructor 
+		private Person(Set<String> phoneNumbers, String firstName, String lastName) {
+			this.phoneNumbers = phoneNumbers;
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
-
+		
+		//Factory method to create new Person instance
+		public static Person getNewPerson(Set<String> phoneNumbers, String firstName, String lastName){
+			return new Person(phoneNumbers, firstName, lastName);
+		}
+		
 		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
+			//Returning immutable Set
+			return Collections.unmodifiableSet(phoneNumbers);
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		
+		//Removed setter methods
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
-		}
+		}*/
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
+		/*public void setFirstName(String newName) {
 			firstName = newName;
-		}
+		}*/
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
+		/*public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
