473eb716658619606c243d00f02816e56984c930
HarishaAkasapu <harisha.akasapu@gmail.com>
2016-12-08 15:00:58 -0500

    Harisha: Made Counter thread-safe

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,27 +1,29 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
-
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-
 */
 public class Exercise4 {
 
 	public static class Counter {
-		
-		private int count = 0;
+		//Using atomic operation for thread safety
+		private AtomicInteger count = new AtomicInteger(0);
 		
 		public int increment() {
-			return ++count;
+			//return ++count;
+			return count.incrementAndGet();
 		}
 		
 		public int getCount() {
-			return count;
+			//return count;
+			return count.get();
 		}
 		
 		public void resetCount() {
-			count = 0;
+			//count = 0;
+			count.set(0);
 		}
-
 	}
 }
