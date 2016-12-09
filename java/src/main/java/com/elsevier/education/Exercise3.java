a59d7fd4bcf574e3bd2813be9c3660ff0b8a3351
Rama Sudalayandi <Rama_Sudalayandi@cable.comcast.com>
2016-12-09 14:25:54 -0500

    Completed by Rama Sudalayandi

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -1,41 +1,44 @@
 package com.elsevier.education;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
 
-can run with gradlew clean build; java -cp build/classes/main com.elsevier.education.Exercise3
+ can run with gradlew clean build; java -cp build/classes/main com.elsevier.education.Exercise3
 
-TODO We should be able to call people.add() twice but end with only one object in it.
+ TODO We should be able to call people.add() twice but end with only one object in it.
 
-*/
+ */
 public class Exercise3 {
 
 	public static void main(String[] args) {
-	
+
 		Set<Person> people = new HashSet<>();
-	
+
 		Person p1 = new Person(1);
 		people.add(p1);
 		people.add(p1);
-		
+
 		System.out.println(people.size());
-		
+
 	}
-	
+
+	/**
+	 * Replacing random generator with a prime number to make the hashcode same for similar objects
+	 */
 	public static class Person {
-	
-		private static Random generator = new java.util.Random();
+
 		private Integer id;
-		
+
 		public Person(int newId) {
 			id = newId;
 		}
-		
+
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id * 31;
 		}
-		
+
 		public boolean equals(Object other) {
 			return id.equals(((Person)other).id);
 		}
--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -2,26 +2,27 @@ package com.elsevier.education;
 
 /**
 
-TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+ TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
-*/
+ */
 public class Exercise4 {
 
+	/**
+	 * The Counter was NOT thread safe.
+	 * Reduced the 2 step process of increment and get to a single get and made it thread safe with synchronized
+	 * Reset is not synchronized because practically there is no need. If the counter is reset by any thread
+	 * the next increment will be 1.
+	 */
 	public static class Counter {
-		
+
 		private int count = 0;
-		
-		public int increment() {
+
+		public synchronized int getCount() {
 			return ++count;
 		}
-		
-		public int getCount() {
-			return count;
-		}
-		
+
 		public void resetCount() {
 			count = 0;
 		}
-
 	}
 }
