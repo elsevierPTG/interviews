7c63e69541dd53ee0f4134e851854edbdb64983e
tdirenna <tdirenna@gmail.com>
2016-12-15 22:03:05 -0500

    Add files via upload

---
+++ dot-net/UnitTesting/WriteUnitTest/Services/LessonServiceUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WriteUnitTest.Services;
+using WriteUnitTest.Repositories;
+
+namespace WriteUnitTest.UnitTests.Services
+{
+    [TestClass]
+    public class LessonServiceUnitTests
+    {
+        /// <summary>
+        /// This will test if updating a lesson to a passing grade will work correctly
+        /// </summary>
+        [TestMethod]
+        public void UpdateLessonGrade_Test()
+        {
+            // set up test vars
+            int testLessonId = 12;
+            double testGrade = 90.2d;
+            bool expectedIsPassed = true; // a grade of 90.2 is passing for lesson 12
+
+            // perform the update on the lesson
+            var lessonSvc = new LessonService();
+            lessonSvc.UpdateLessonGrade(testLessonId, testGrade);
+
+            // get the updated lesson
+            var lesson = lessonSvc.lessonRepo.GetLesson(testLessonId);
+
+            // test if it worked by comparing the expected 
+            Assert.AreEqual(expectedIsPassed, lesson.IsPassed);
+        }
+    }
+}
