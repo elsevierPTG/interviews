9c55520a7550a2914a908a220989e0165c710d5c
Nandeeswar <m.nandeeswar@gmail.com>
2016-12-16 16:20:58 +0530

    Now the class will act as singleton
    only one object will be created.

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,13 +8,24 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+
+		private static Singleton instance = null;
+		
+		public static Singleton getInstance() {
+      			if(instance == null) {
+         			instance = new Singleton();
+      			}
+      			return instance;
+   		}
+
 		public void doSomeThing() {
 			System.out.println("Doing something....");
-	    }
+	   	 }
 	}
+
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
