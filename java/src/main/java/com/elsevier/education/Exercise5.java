b990524aaf3c9512893fbd0792c0c2d7f930c822
616071 <DINESHKUMAR.R8@cognizant.com>
2016-12-20 16:57:54 +0530

    Exercise5-Singleton
    a)make the constructors private it restrict the instantiation of the class.
    b)Apply eager instantiation .

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,20 +1,29 @@
 package com.elsevier.education;
 
 /**
-
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
-
 */
+
 public class Exercise5 {
 	
 	public static class Singleton {
+	
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton(){
+	}
+	
+	public static Singleton getSingletonInstance(){
+		return singleton;
+	}
+
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getSingletonInstance();
 		st.doSomeThing();
 	}
 }
