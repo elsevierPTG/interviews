f3e8579d9b779dd4341cb3d09e8ff51718fab2a0
git4dkumaran <mail2dkumaran@gmail.com>
2016-12-19 22:54:28 -0800

    updated with solution

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,26 +1,26 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
-
-TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-
-*/
+ * TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+ */
 public class Exercise4 {
 
 	public static class Counter {
-		
-		private int count = 0;
-		
+
+		private AtomicInteger count = new AtomicInteger(0);
+
 		public int increment() {
-			return ++count;
+			return count.incrementAndGet();
 		}
-		
+
 		public int getCount() {
-			return count;
+			return count.get();
 		}
-		
+
 		public void resetCount() {
-			count = 0;
+			count.set(0);
 		}
 
 	}
--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,19 +1,29 @@
 package com.elsevier.education;
 
 /**
-
-TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
-
-*/
+ * TODO: Turn the "Singleton" class into an actual singleton. The main() method
+ * should still call .doSomething().
+ */
 public class Exercise5 {
-	
+
 	public static class Singleton {
+
+		private static final Singleton INSTANCE = new Singleton();
+
+		private Singleton() {
+
+		}
+
+		public Singleton getInstance() {
+			return INSTANCE;
+		}
+
 		public void doSomeThing() {
 			System.out.println("Doing something....");
-	    }
+		}
 	}
-		
-	public static void main(String a[]){
+
+	public static void main(String a[]) {
 		Singleton st = new Singleton();
 		st.doSomeThing();
 	}
