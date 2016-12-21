6471b5ed5b92e901bb9571e281c266253c675b49
dev <lucky.njokweni@gmail.com>
2016-12-21 13:42:53 -0500

    Lucky Njokweni : lucky.njokweni@gmail.com

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,40 +1,41 @@
 package com.elsevier.education;
-
+import java.util.Set;
 /**
 
 TODO: Make this class immutable.
-
+We accomplish this as follows:
+ - Make the class final
+ - Make all member variables final, since they are already private
+ - Remove setter methods
+ - Modify constructor to accept arguments to initialize member variables
 */
 public class Exercise1 {
 
-	public static class Person {
+	// Make it final
+	final public static class Person {
+		// Make all members private
+		final private Set<String> phoneNumbers;
+		final private String firstName;
+		final private String lastName;
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+		// Modifiy constructor to accept parameters to initialize member variables
+		public Person(Set<String> mPhoneNumbers, String mFirstName, String mLastName) {
+			// initialize fields
+			this.phoneNumbers = mPhoneNumbers;
+			this.firstName = mFirstName;
+			this.lastName = mLastName;
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
 		
 		public String getLastName() {
 			return lastName;
-		}
-		public void setLastName(String newName) {
-			lastName = newName;
 		}
 	}
 }
--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,13 +8,31 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		private static Singleton instance;
+		private Singleton(){}
+		
+		private static Singleton getInstance()
+		{
+			if(instance==null)
+			{
+				synchronized(Singleton.class)
+				{
+					if(instance == null)
+					{
+						instance = new Singleton();
+					}
+				}
+			}
+			return instance;
+		}
+		
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
