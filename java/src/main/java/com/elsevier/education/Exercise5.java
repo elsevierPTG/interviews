edeedb9888693bf76e7aa34cf35b8f16f09fab2e
nprabakar <Prabakaran.natarajan@cognizant.com>
2017-01-23 09:35:58 -0500

    Update Exercise5.java

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -7,6 +7,21 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 */
 public class Exercise5 {
 	
+	/** create static final object to not change the created object **/
+	private static final Singleton singleton = new Singleton();
+		
+	/** create private constructor for denying direct access from outside**/
+	private Singleton(){
+		/** throw exception in private constructor to avoid multiple creation using reflection**/
+		if(singleton != null){
+			throw new IllegalStateException("instance already created.");
+		}
+	 }
+	
+	/** create static method to get the instance created**/
+	public static Singleton getInstance(){
+		return singleton;
+	}
 	public static class Singleton {
 		public void doSomething() {
 			System.out.println("Doing something....");
