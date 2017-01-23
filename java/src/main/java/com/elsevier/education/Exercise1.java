5e85d6edfd53ee0f3796dcd173c256b0635b481f
nprabakar <Prabakaran.natarajan@cognizant.com>
2017-01-23 09:23:03 -0500

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -8,35 +8,32 @@ TODO: Make this class immutable.
 
 */
 public class Exercise1 {
-
+	/** Make class  final so that it cannot be extended**/
 	public static class Person {
+		/** Make all fields private so that direct access is not allowed**/
+		/** Make all fields final so that it cannot be modified**/
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+		/** initialize phoneNumbers using a new set , so that the original one is not changed**/
+		public Person(String firstName, String lastName, Set<String> phoneNumbers) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.phoneNumbers = return new HashSet<String>(phoneNumbers);
 		}
-
+		/** Get phoneNumbers in a new set , so that the original one is not changed**/
 		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
+			return new HashSet<String>(phoneNumbers);
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
+				
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
+				
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		
 	}
 }
