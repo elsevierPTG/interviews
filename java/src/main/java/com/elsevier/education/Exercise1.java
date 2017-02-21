9c67dd5a459249988ff66d37be2808f75b981428
pratibhasakunde <pratibha.sakunde@cognizant.com>
2017-02-21 18:00:05 +0530

    Update Exercise1.java
    1)declared class Person as final
    2)Removed setters
    3)made all fields final & private
    4)Made constructor private will ensure no unplanned construction of class
    5)factory method to store object creation at single place

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -7,37 +7,45 @@ import java.util.Set;
 TODO: Make this class immutable.
 
 */
+/**1)declared class Person as final2)Removed setters3)made all fields final & private4)Made constructor private will ensure no unplanned construction of class5)factory method to store object creation at single place
+*/
 public  class Exercise1 {
 
 	public static final class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		
+		private Person(Set<String phoneNumbers,String fisrtName,String lastName>){
+			this.phoneNumbers=phoneNumbers;
+			this.firstName=fisrtName;
+			this.lastName=lastName;
 		}
-
+		public static Person createNewInstance(Set<String> phoneNos,String firstNm,String lastNm){
+			return new Person(phoneNos,fisrtNm,lastNm);
+		}
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
 		
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
-		}
+		}*/
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
+		/*public void setFirstName(String newName) {
 			firstName = newName;
-		}
+		}*/
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
+		/*public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
