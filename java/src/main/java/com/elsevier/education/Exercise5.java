5e24a029d325643811365f9dd88e75684c1078c7
pratibhasakunde <pratibha.sakunde@cognizant.com>
2017-02-21 19:09:26 +0530

    Update Exercise5.java

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,23 +8,23 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
-		private static Singleton instance=null;
+		private static final Singleton instance = new Singleton();	//creat class instance	
+		//added private constructor
 		private Singleton(){
+			
 		}
 		public void doSomething() {
 			System.out.println("Doing something....");
-	    }
-		public static Singleton getInstance(){
-			if(instance==null){
-				instance=new Singleton();
-			}
+	    	}
+		public static Singleton getInstance(){//added method to get single instance
+			
 			return instance;
 		
 		}
 		
 	public static void main(String a[]){
 		//Singleton st = new Singleton();
-		Singleton st=Singleton.getInstance();
+		Singleton st=Singleton.getInstance();//call class method here to get class instance
 		st.doSomeThing();
 		
 		
