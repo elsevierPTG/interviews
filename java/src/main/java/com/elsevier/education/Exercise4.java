3b3245472134fe6e3641d97174106d41e832094b
pratibhasakunde <pratibha.sakunde@cognizant.com>
2017-02-21 19:06:12 +0530

    Update Exercise4.java

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -4,25 +4,29 @@ package com.elsevier.education;
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
-*/
+/**   
+No, the counter is not threadsafe because counter can be updated by increment() and resetCount() methods,  
+by 2 different threads and return very inconsistent results.  
+To fix this, I'd use AtomicInteger instead of a primitive int as the counter type. */ 
+import java.util.concurrent.atomic.AtomicInteger;
 public class Exercise4 {
 
 	public static class Counter {
 		
-		private int count = 0;
+		private AtomicInteger count = new AtomicInteger(0);
 		
 		
-		public synchronized int increment() {
-			return ++count;
-		}
+		public int increment() {
+			return count.incrementAndGet(); 		
+}
 		
 		public synchronized int getCount() {
-			return count;
-		}
+			return count.get(); 		
+}
 		
 		public synchronized void resetCount() {
-			count = 0;
-		}
+			count.set(0); 		
+}
 
 	}
 }
