0dc75186049d917013d2957c8c30d2cce7f10b6b
pratibhasakunde <pratibha.sakunde@cognizant.com>
2017-02-21 18:47:09 +0530

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,51 +1,52 @@
 package com.elsevier.education;
 
 import java.util.Set;
+import java.util.Collections;
+import java.util.stream.Collectors; 
 
-/**
-
-TODO: Make this class immutable.
-
-*/
-/**1)declared class Person as final2)Removed setters3)made all fields final & private4)Made constructor private will ensure no unplanned construction of class5)factory method to store object creation at single place
-*/
 public  class Exercise1 {
+	/**
+	TODO:Make this class immutable
+	*/
 
-	public static final class Person {
+	public final static class Person {  //make class as final
 		
+		//set all varibales as final
 		private final Set<String> phoneNumbers;
 		private final String firstName;
 		private final String lastName;
 		
-		
-		private Person(Set<String phoneNumbers,String fisrtName,String lastName>){
-			this.phoneNumbers=phoneNumbers;
+		public person(){
+		}
+		public Person(final Set<String phoneNumbers,final String fisrtName,final String lastName>){
+			this.phoneNumbers = phoneNumbers.stream().collect(Collectors.toSet()); 
 			this.firstName=fisrtName;
 			this.lastName=lastName;
 		}
-		public static Person createNewInstance(Set<String> phoneNos,String firstNm,String lastNm){
-			return new Person(phoneNos,fisrtNm,lastNm);
-		}
+		
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
+		//unmodifiableset will create read only set of phoone numbers
+		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+			phoneNumbers=newPhoneNumbers;
+			return Collections.unmodifiableSet(phoneNumbers); 		
+}
 		
-		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}*/
-		
+		/**Since Strings are immutable no need to do anything
+		*/
 		public String getFirstName() {
 			return firstName;
 		}
-		/*public void setFirstName(String newName) {
+		public void setFirstName(String newName) {
 			firstName = newName;
-		}*/
+		}
 		
 		public String getLastName() {
 			return lastName;
 		}
-		/*public void setLastName(String newName) {
+		public void setLastName(String newName) {
 			lastName = newName;
-		}*/
+		}
 	}
 }
