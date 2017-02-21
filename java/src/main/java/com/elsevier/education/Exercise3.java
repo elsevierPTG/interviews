a42d2b5bb2bf62f267356d7c5aadcf9d1c7ec5b6
rupinkamat <rupin.kamat@sungard.com>
2017-02-21 16:49:36 +0530

    Exercise3.java
    Rupin Kamat : rupinkamat@gmail.com

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -9,19 +9,25 @@ TODO Examine the failing test case for this class.
  We can test with "gradlew test"
 
 */
+
+// Considering people is a hashset
+// people.add(new Person(2));
+// people.add(new Person(2));
+// people.size() will still be 1. So Person with ID 2 will be added only once
 public class Exercise3 {
 
-	public static class Person {
+	public class Person {
 	
-		private static Random generator = new java.util.Random();
+		private static Random generator = new java.util.Random(); // Not utilizing generator
 		private Integer id;
 		
 		public Person(int newId) {
 			id = newId;
 		}
 		
+		// id value alone should be used  for a hashcode in order to satisfy the problem statement
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id.hashCode();
 		}
 		
 		public boolean equals(Object other) {
