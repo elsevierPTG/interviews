f311462380fa8551b182571f9efd4fc34ca0aead
poornisha <p.pannirselvam@elsevier.com>
2017-02-27 19:38:07 +0530

    Update Exercise2.java
    1.Create a interface Engine 
    2.Make GasEngine and ElectricEngine classes to implements Engine interface
    3.In car constructor need to pass engine object which needs to be injected

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -8,22 +8,34 @@ TODO make sure we have no-op implementations of the gas engine and electric engi
 
 */
 public class Exercise2 {
+	
+	public interface Engine{
+		default void spinWheels(){
+			//no-op for now
+		}
+	}
 
 	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
+		private final Engine engine;
+		public Car(Engine engine){
+			this.engine = engine;
 		}
 		
 		public void moveForward() {
-			engine.spinWheels();
+		engine.spinWheels();
 		}
 	}
 	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
+	public static class GasEngine implements Engine {
+	}
+	public static class ElectricEngine implements Engine {
+	}
+	
+	public static void main(String[] args) {
+		Car carGasEngine = new Car(new GasEngine());
+		carGasEngine.moveForward();
+		
+		Car carElectricEngine = new Car(new ElectricEngine());
+		carElectricEngine.moveForward();
 	}
 }
