ab4675a8081da59f0d50ca78876c22c5c2648d3c
jinwchen <jinwchen@yahoo.com>
2017-02-27 13:34:12 -0500

    Made the changes to meet the requirements.

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,6 +1,12 @@
 package com.elsevier.education;
 
 /**
+Candidate : Jin W. Chen
+Email : JinWChen@yahoo.com
+recruiter :
+Linda Pham | Technical Recruiter
+JOBSPRING PHILADELPHIA 
+Office: (267) 765-6100
 
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
 
@@ -8,13 +14,23 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		
+		private static Singleton singleton;
+		
 		public void doSomething() {
 			System.out.println("Doing something....");
 	    }
+		
+		public static synchronized Singleton getInstance( ) {
+			if (singleton == null) {
+				singleton = new Singleton();
+			}
+			return singleton;
+		}
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
+		Singleton st = Singleton.getInstance();
+		st.doSomething();
 	}
 }
