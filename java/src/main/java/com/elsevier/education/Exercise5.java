a1d90844c5436b00155f87d549552e8874a70198
WaseemCognizant <waseembasha.s@cognizant.com>
2017-03-02 17:54:23 +0530

    Update Exercise5.java
    Make constructor as private as we can't able to create object outside the class thus only one instance of a class is created. Create getInstance() method to get the instance.

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,13 +8,33 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		// By default Singleton is initialized to null
+		private static Singleton singleton;
+
+		//Constructor is made private to prevent any other class from instantiating.
+		private Singleton(){
+		}
+		
 		public void doSomething() {
 			System.out.println("Doing something....");
 	    }
+		
+		/*
+		 This method will create and returns Singleton instance only if singleton is null else it will return 
+		 the old instance which was already created
+		 */
+		public synchronized static Singleton getInstance() {
+			//create new instance only if singleton is null
+			if(singleton == null){
+				singleton = new Singleton();
+			}
+			return singleton;
+		}
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		//Singleton.getInstance() is used to get the Singleton instances
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
