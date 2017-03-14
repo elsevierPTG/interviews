1f5add0aa2839edebf031cb6e9f87bad5b2e225e
Arunkalahastri <Arun.kalahastri@cognizant.com>
2017-03-14 17:44:31 +0530

    Make class Immutable
    1.Make class as well as the variables final
    2.Parameterized constructor to initial the variables.
    3.Since the variables are final we should not have setter methods

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -8,35 +8,30 @@ TODO: Make this class immutable.
 
 */
 public class Exercise1 {
-
-	public static class Person {
+	//Make class as well as the variables final
+	public final static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
+		//Parameterized constructor to initial the variables
+		public Person(String firstName,String lastName,Set<String> phoneNumbers) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.phoneNumbers = phoneNumbers;
 		}
-
+		//Since the variables are final we should not have setter methods
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
+				
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
+				
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		
 	}
 }
