31ee7c0e7f1ba097a8068ec2322fd26a36e5ee7d
Arunkalahastri <Arun.kalahastri@cognizant.com>
2017-03-14 17:53:05 +0530

    Singleton class
    1) The class should not be a static class
    2) Private constructor to control object creation from other class
    3) Created getInstance() method to get the instance

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -7,14 +7,30 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 */
 public class Exercise5 {
 	
-	public static class Singleton {
+	public class Singleton {
+		private static Singleton singleton;
+		//Private constructor to control object creation from other class
+		private Singleton() {
+		}
+		
+		public synchronized static Singleton getInstance() {
+			//check for null
+			if (singleton == null) {
+				//if null creates a  new instance
+				singleton = new Singleton();
+			}
+			//if not null returns the old object
+			return singleton;
+		}
+		
 		public void doSomething() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		//getInstatbnce is ussed to get the singleton instance
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
