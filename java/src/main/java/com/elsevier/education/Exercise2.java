9aea02387b59f3b9bb64b055bf779aecea4c6c88
Arunkalahastri <Arun.kalahastri@cognizant.com>
2017-03-14 17:59:38 +0530

    Dependency Injection
    1. Create a interface Engine 
    2. Create GasEngine and ElectricEngine classes that implement Engine interface
    3. In Car constructor need to pass engine object which needs to be injected

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -10,20 +10,41 @@ TODO make sure we have no-op implementations of the gas engine and electric engi
 public class Exercise2 {
 
 	public static class Car {
+		private Engine engine;
 		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
+		// Need to pass Engine object need to be injected
+		public Car(Engine e) {
+			this.engine = e;
 		}
 		
 		public void moveForward() {
 			engine.spinWheels();
 		}
 	}
-	
-	public static class GasEngine {
+	//Both GasEngine and ElectricEngine implements Engine interface
+	public static class GasEngine implements Engine{
+		@Override
 		public void spinWheels() {
 			// no-op for now
+			System.out.println("inside gas engine");
 		}
+	}
+	
+	public static class ElectricEngine implements Engine {
+		@Override
+		public void spinWheels() {
+			// no-op for now
+			System.out.println("inside electric engine");
+		}
+	}
+	
+	//Created engine Interface
+	public static interface Engine {
+		public void spinWheels();
+	}
+	
+	public static void main(String args[]) {
+		Car car = new Car(new GasEngine());
+		car.moveForward();
 	}
 }
