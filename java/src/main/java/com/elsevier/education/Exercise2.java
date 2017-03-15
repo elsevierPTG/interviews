efcbd656b5e708e4c0abb9edcca2e6883f3d1e09
nickcianci <n.cianci3@gmail.com>
2017-03-15 19:23:26 -0400

    Add Exercise2 dependency injection solution

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -9,19 +9,45 @@ TODO make sure we have no-op implementations of the gas engine and electric engi
 */
 public class Exercise2 {
 
+	/**
+	 * We use constructor dependency injection. When an instance of Car is
+	 * created, it must be provided with some or any kind of Engine (Gas or Electric).
+	 *<p>
+	 * This allows for loose-coupling: Car is not dependent on a specific Engine
+	 * implementation. Instead it is more flexible because it can be provided with any kind of Engine type.
+	 */
 	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
+
+		private Engine engine;
+
+		public Car(Engine engine) {
+			this.engine = engine;
 		}
-		
+
 		public void moveForward() {
 			engine.spinWheels();
 		}
 	}
+
+	/**
+	 * Defined Engine interface that acts as a common abstract type for GasEngine
+	 * and ElectricEngine.
+	 *<p>
+	 * Both types of engine implement the interface.
+	 */
+	public static interface Engine {
+		void spinWheels();
+	}
+
+	public static class GasEngine implements Engine {
+		@Override
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
 	
-	public static class GasEngine {
+	public static class ElectricEngine implements Engine {
+		@Override
 		public void spinWheels() {
 			// no-op for now
 		}
