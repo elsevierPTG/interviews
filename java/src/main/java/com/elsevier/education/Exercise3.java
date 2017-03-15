328103aa810253622f3aec9cf229818b8a970d74
nickcianci <n.cianci3@gmail.com>
2017-03-15 19:24:57 -0400

    Add Exercise3 hashCode solution

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -1,7 +1,5 @@
 package com.elsevier.education;
 
-import java.util.*;
-
 /**
 
 TODO Examine the failing test case for this class.
@@ -11,21 +9,54 @@ TODO Examine the failing test case for this class.
 */
 public class Exercise3 {
 
+	/**
+	 * The code was refactored to use a well-defined implementation of hashCode and equals.
+	 * <p>
+	 * The main problem with the previous hashCode implementation was that it
+	 * would return different hashCode values for "equal" Person instances
+	 * (when the id's are the same value).
+	 * <p>
+	 * Collections depend on the property that if two objects are
+	 * "equal", then they must generate the same hashCode values.
+	 * <p>
+	 * So the test initially failed because when calling add on Set, it first
+	 * checks the hashCode of the object. If there are no object's in the set
+	 * with that hashCode, then it will add it to the set. Since the two objects
+	 * generated different hashCodes, the Set contained 2 elements instead of the expected 1.
+	 * <p>
+	 * Now the two instances will generate the same hashCode, will be equal, and
+	 * so the second call to add will not add an element to the Set.
+	 */
 	public static class Person {
-	
-		private static Random generator = new java.util.Random();
 		private Integer id;
-		
+
 		public Person(int newId) {
 			id = newId;
 		}
-		
+
+		@Override
 		public int hashCode() {
-			return id * generator.nextInt();
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
 		}
-		
-		public boolean equals(Object other) {
-			return id.equals(((Person)other).id);
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (!(obj instanceof Person))
+				return false;
+			Person other = (Person) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
 		}
 	}
 }
