ae29f1e13012801651d7546076c4e9725dac9844
nickcianci <n.cianci3@gmail.com>
2017-03-15 19:26:22 -0400

    Add Exercise4 thread-safe solution

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,5 +1,7 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
@@ -7,20 +9,32 @@ TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 */
 public class Exercise4 {
 
+	/**
+	 * In Java, primitives and Objects are not inherently thread-safe.
+	 * If 2 threads read the value of a primitive. One thread increments the
+	 * value by 1 and the other thread increments the value by 2. Then the
+	 * threads will be out of sync.
+	 * <p>
+	 * There are several ways to address this behavior.
+	 * <p>
+	 * One way is to use the built-in classes from the concurrent library.
+	 * AtomicInteger is a built-in int wrapper that is thread-safe.
+	 *
+	 */
 	public static class Counter {
-		
-		private int count = 0;
-		
+
+		private AtomicInteger count = new AtomicInteger();
+
 		public int increment() {
-			return ++count;
+			return count.incrementAndGet();
 		}
-		
+
 		public int getCount() {
-			return count;
+			return count.get();
 		}
-		
+
 		public void resetCount() {
-			count = 0;
+			count.set(0);
 		}
 
 	}
