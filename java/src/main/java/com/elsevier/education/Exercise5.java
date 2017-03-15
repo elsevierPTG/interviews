b65f77bffbf09af364c2694f63ac0112da437544
nickcianci <n.cianci3@gmail.com>
2017-03-15 19:28:58 -0400

    Add Exercise5 singleton solution

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -6,15 +6,48 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 
 */
 public class Exercise5 {
-	
-	public static class Singleton {
+
+	/**
+	 * The generally accepted way of implementing a singleton is by
+	 * defining an enum with a single instance. This prevents multiple instances
+	 * from being created.
+	 * <p>
+	 * An alternative is to define a private constructor and define a private
+	 * static singleton instance. The instance is returned when getInstance is called.
+	 * <p>
+	 * The enum implementation has additional advantages that I don't recall.
+	 */
+	public static enum Singleton {
+
+		INSTANCE;
+
+		public static Singleton getInstance() {
+			return INSTANCE;
+		}
+
 		public void doSomething() {
 			System.out.println("Doing something....");
-	    }
+		}
 	}
-		
+
+	public static class SingletonAlt {
+
+		private static final SingletonAlt INSTANCE = new SingletonAlt();
+
+		private SingletonAlt() {
+		}
+
+		public static SingletonAlt getInstance() {
+			return INSTANCE;
+		}
+
+		public void doSomething() {
+			System.out.println("Doing something....");
+		}
+	}
+
 	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
+		Singleton st = Singleton.getInstance();
+		st.doSomething();
 	}
 }
