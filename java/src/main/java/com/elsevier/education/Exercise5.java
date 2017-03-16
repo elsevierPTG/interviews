60590c9a02b3290274b40241c612932141647611
antonvinodkumar95 <Anton.VinodKumar@cognizant.com>
2017-03-16 11:48:14 +0530

    Update Exercise5.java

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -7,14 +7,29 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 */
 public class Exercise5 {
 	
-	public static class Singleton {
+	public final static class Singleton {
+		private final static Singleton st=new Singleton();
+		public static Singleton getInstance(){
+			if(st!=null){
+				throw new IllegalStateException("Instance already created");
+			}
+			else{
+				return st;
+			}
+		//Check is being done to explore whether an object is duplicated.
+		//If yes,then singleton property is violated.
+		private Singleton{
+		}
+		//We make constructor as private because: 1.We cannot create object outside of the class.
+                //                                        2.Singleton cannot allow any duplication in creating the instance of the class.
+
 		public void doSomething() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
+		Singleton.getInstance().doSomething();
+		
 	}
 }
