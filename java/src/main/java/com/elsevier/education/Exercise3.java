4c019f310369b0a24319ec58073d1465c62cba74
SumalathaG <sumalatha.g@cognizant.com>
2017-03-16 16:24:58 +0530

    Update Exercise3.java

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -12,16 +12,20 @@ TODO Examine the failing test case for this class.
 public class Exercise3 {
 
 	public static class Person {
-	
-		private static Random generator = new java.util.Random();
+		// Not required to use a ramdom generator
+		//private static Random generator = new java.util.Random();
 		private Integer id;
 		
 		public Person(int newId) {
 			id = newId;
 		}
-		
+		/* The generator.nextInt() would provide a different number all the
+		time hence the hashcode would be different in each case ,
+		so the set would allow to add the  objects even when they are equal. 
+		To call people.add() twice but end with only one object in it have modified the code to 
+		return same hashcode for same objects  */
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id * 1;
 		}
 		
 		public boolean equals(Object other) {
