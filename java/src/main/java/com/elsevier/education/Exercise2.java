57f49fedda9a0d76f9d144b617377242932b5a7c
antonvinodkumar95 <Anton.VinodKumar@cognizant.com>
2017-03-16 12:24:01 +0530

    Update Exercise2.java

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -8,22 +8,36 @@ TODO make sure we have no-op implementations of the gas engine and electric engi
 
 */
 public class Exercise2 {
-
+        public interface Engine {
+                default void spinWheels() {
+        // no-op for now
+             }
+      }
 	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
+		//To create dependency injection we do the following: 1.To create a static class Car.
+                //                                                    2.This class can be used to create an object for the class Engine i.e., outside of it to maintain dependency injection.
+ 
+		private final Engine engine;
+                public Car(Engine engine) {
+                this.engine = engine;
+             }
 		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
+                engine.spinWheels();
+             }
+      } 
+	        public static class GasEngine implements Engine {
+             }
+
+                public static class ElectricEngine implements Engine {
+             }
+	        //To create an appropriate abstraction we need: 1.To create an interface Engine.
+                //                                              2.To create static classes GasEngine and 
+		
 	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
-}
+	        public static void main(String[] args) {
+                Car car1 = new Car(new GasEngine());
+                car1.moveForward();
+                Car car2 = new Car(new ElectricEngine());
+                car2.moveForward();
+               }
+       }
