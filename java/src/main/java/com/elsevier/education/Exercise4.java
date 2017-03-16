0dbfe98be545cdfcfcb40cd89af14850277b5e82
antonvinodkumar95 <Anton.VinodKumar@cognizant.com>
2017-03-16 12:29:12 +0530

    Update Exercise4.java

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -11,17 +11,21 @@ public class Exercise4 {
 		
 		private int count = 0;
 		
-		public int increment() {
+		public synchronized int increment() {
 			return ++count;
 		}
 		
-		public int getCount() {
+		public synchronized int getCount() {
 			return count;
 		}
 		
-		public void resetCount() {
+		public synchronized void resetCount() {
 			count = 0;
 		}
+		
+                //It's not thread-safe.
+                //So we do the following to make it threadsafe:1.We can use synchronized modifier for all methods.
+                //                                             2.Variable "count" can be randomly changed from multiple threads,so we need to maintain thread safety using synchronization.
 
 	}
 }
