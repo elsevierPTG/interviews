2518ad877f12f0903fe85523ded9413873e305a6
Philip Preston <philip.preston@gmail.com>
2017-03-22 20:12:12 -0400

    Exercises completed

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -5,6 +5,13 @@ package com.elsevier.education;
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
 */
+/**
+ * Counter is not thread-safe. One thread could use increment or resetCount to affect what another
+ * sees when trying to call getCount.
+ *
+ * To fix this we could mark the `count` variable as volatile, so Java knows it could be used
+ * by multiple threads, and won't copy the value between them.
+ */
 public class Exercise4 {
 
 	public static class Counter {
