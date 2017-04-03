612f4010f4ab3de36d3191a64be68318d3e8a4aa
Safura Harmain <harmain.java06@gmail.com>
2017-04-03 01:23:29 -0400

    Commiting java files

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,42 +1,60 @@
 package com.elsevier.education;
 
+
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
-
 TODO: Make this class immutable.
-
 */
+/**
+ * Do not provide setter methods to all the properties in class. Setter methods are meant to change the state of object and this is what we want to prevent here.
+ * Make all the fields private , so that no other classes can access from outside and make them final .
+ * We should not allow subclasses to override ,to achieve this make the class as final.
+ * @author Safura
+ *
+ */
 public class Exercise1 {
 
-	public static class Person {
+	public final static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		public Person(String firstName, String lastName, Set<String> tempPhoneNumbers) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			Set<String> tempSet = new TreeSet<>(tempPhoneNumbers);
+			phoneNumbers = tempSet;
 		}
 
 		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
+			return (Set<String>) ((TreeSet<String>)phoneNumbers).clone();
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
+		
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
+		
 		
 		public String getLastName() {
 			return lastName;
 		}
+		
+		/**
+		 * Commented setters to prevent the value modification from outside
+		 */
+		
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+			phoneNumbers = newPhoneNumbers;
+		}
+		public void setFirstName(String newName) {
+			firstName = newName;
+		}
 		public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,21 +1,25 @@
 package com.elsevier.education;
 
 /**
-
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-
 */
+/**
+ * Adding synchronized to this method will makes it thread-safe.
+ *  When synchronized is added to a static method, the Class object is the object which is locked.
+ * @author Safura
+ *
+ */
 public class Exercise4 {
 
 	public static class Counter {
 		
-		private int count = 0;
+		private static int count = 0;
 		
-		public int increment() {
+		public static synchronized int increment() {
 			return ++count;
 		}
 		
-		public int getCount() {
+		public static synchronized int  getCount() {
 			return count;
 		}
 		
