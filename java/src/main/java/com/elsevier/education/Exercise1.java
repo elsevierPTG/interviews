d05af8c1d58e8a3b6a4fa6f22d98bfa5a475f408
daniel2009 <ldy2009@hotmail.com>
2017-04-06 14:15:44 -0400

    add the comments

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 /**
 
 TODO: Make this class immutable.
+Remove the setter methods, and then there is no other ways to modified its member variables once the object is initialized.
 
 */
 public class Exercise1 {
