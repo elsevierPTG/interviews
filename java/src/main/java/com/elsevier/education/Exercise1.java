03ede088160b0bb843da725e185c2acadbf774f5
santosh <santosh.kaarki@gmail.com>
2017-04-06 18:29:52 -0500

    Exercise1 Done!!!

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -9,34 +9,43 @@ TODO: Make this class immutable.
 */
 public class Exercise1 {
 
-	public static class Person {
+	//Declare class as final so it cant be extended.
+		public static final class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+			//Make mutable fields final so its value can be assigned only once.
+			private final Set<String> phoneNumbers;
+			private final String firstName;
+			private final String lastName;
 		
-		public Person() {
-		}
+			//Initialize the field via constructor
+			public Person(Set<String> phone, String fName, String lName) {
+			this.firstName = fName;
+			this.lastName = lName;
+			set<String> tempSet = new TreeSet<>(phone)
+			phoneNumbers = tempSet;
+			}
 
-		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
-		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+			public Set<String> getPhoneNumbers() {
+				return (Set<String>) ((TreeSet<String>)phoneNumbers).clone();
+			}
+		
+			//no setter methods 
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
-		}
+		}*/
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
+		/*public void setFirstName(String newName) {
 			firstName = newName;
-		}
+		}*/
 		
 		public String getLastName() {
 			return lastName;
-		}
+		/*}
 		public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
