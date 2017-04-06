bb83beee52370a5e7e0f6a6b6d30e7a58c8850f5
daniel2009 <ldy2009@hotmail.com>
2017-04-06 14:36:58 -0400

    use abstraction for two types of engines

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -7,23 +7,56 @@ TODO allow use of either a gas engine or electric engine (create an appropriate 
 TODO make sure we have no-op implementations of the gas engine and electric engine
 
 */
+
+/*
+
+Use abstraction for two types of engines and simple fatory pattern to choose engines
+
+*/
 public class Exercise2 {
 
-	public static class Car {
+	public class Car {
 		
-		private GasEngine engine = new GasEngine();
+		public class Engine {
+			public void spinWeels() {
+			}
+		}
+		
+		public class GasEngine extends Engine {}
+		public class EleEngine extends Engine {}
+		
+		public class EngineSelector {
+			Engine selectEngine(String type) {
+				switch(type) {
+					case "gas":
+						return new GasEngine();
+					case "electric":
+						return new EleEngine();
+					default:
+						break;
+				}
+				return null;
+			}
+		}
+		
+		private EngineSelector engineSelector = null;
+		private Engine engine = null;
 		
 		public Car() {
+			engineSelector = new EngineSelector();
+			engine = engineSelector.selectEngine("gas");
+		}
+		
+		public Car(String type) {
+			engineSelector = new EngineSelector();
+			engine = engineSelector.selectEngine(type);
 		}
 		
 		public void moveForward() {
-			engine.spinWheels();
+			if (engine != null) {
+				engine.spinWheels();
+			}
 		}
 	}
 	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
 }
