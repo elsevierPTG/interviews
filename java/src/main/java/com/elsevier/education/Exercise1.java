6de927458ee198f2f2b6475e5c8ba5e888cba96a
bsriramya <bsriramya@gmail.com>
2017-04-07 08:29:16 -0400

    Update Exercise1.java
    Removed Setter  Methods,Made all fields private and final. Declared the class final.And the constructor was made private.

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -7,36 +7,35 @@ import java.util.Set;
 TODO: Make this class immutable.
 
 */
-public class Exercise1 {
+/**
+
+Removed Setter  Methods,Made all fields private. Declared the class final.And the constructor was made private.
+
+*/
+public final class Exercise1 {
 
 	public static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private  final String firstName;
+		private  final String lastName;
 		
-		public Person() {
+		private Person() {
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
+		
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
+	
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+	
 	}
 }
