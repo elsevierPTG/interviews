1b0d0f676c4a5c799d3ab8b6857db1ceb873a242
HariniChowdary <chowdary.harini.l@gmail.com>
2017-04-10 18:58:52 -0400

    HariniChowdary-04102017

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,27 +1,41 @@
 package com.elsevier.education;
 
 /**
-
 TODO refactor the Car to use dependency injection of the engine
 TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
 TODO make sure we have no-op implementations of the gas engine and electric engine
-
 */
 public class Exercise2 {
+	// Interface for engine
+	public interface Engine {
+		public void spinWheels();
+	}
 
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
+	public static class Car {		
+		private Engine engine;
 		
 		public Car() {
 		}
+
+		// Assigns engine based on the engine instance we pass to the method
+		public void setEngine(Engine engine) {
+			this.engine = engine;
+		}
 		
 		public void moveForward() {
-			engine.spinWheels();
+			this.engine.spinWheels();
 		}
 	}
 	
-	public static class GasEngine {
+	// Class implements Engine Interface
+	public static class GasEngine implements Engine {
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
+
+	// Class implements Engine Interface
+	public static class ElectricEngine implements Engine {
 		public void spinWheels() {
 			// no-op for now
 		}
--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,20 +1,32 @@
 package com.elsevier.education;
 
 /**
-
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
-
 */
 public class Exercise5 {
 	
-	public static class Singleton {
-		public void doSomething() {
+	public static class Singleton 
+    {
+		// Variable stores singleton object
+		private static Singleton singleton = new Singleton();
+
+		// Constructor declared as private to make the class singleton
+        private Singleton() {
+
+        }
+
+		public static void doSomething() {
 			System.out.println("Doing something....");
 	    }
+
+		// Returns the singleton instance
+        public static Singleton getInstance() {
+            return singleton;
+        }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
