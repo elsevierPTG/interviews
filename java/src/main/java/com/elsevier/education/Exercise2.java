c3aa68a6ec0341fde4590afb36b08e3e5758bd49
smrutigadag <smruti.gadag@gmail.com>
2017-04-19 16:51:34 -0400

    Update Exercise2.java

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,29 +1,56 @@
 package com.elsevier.education;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 /**
 
-TODO refactor the Car to use dependency injection of the engine
+TODO refactor the Car to use dependency injection of the engine 
 TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
 TODO make sure we have no-op implementations of the gas engine and electric engine
+
+Not sure what you mean by no-op implementations.
 
 */
 public class Exercise2 {
 
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
-	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
+  public static class Car {
+    
+    @Autowired(required = true)
+    @Qualifier("GasEngine")
+    private Engine engine;
+        
+    public Car() {
+    }
+    
+    public void moveForward() {
+      engine.spinWheels();
+    }
+  }
+  
+  public static class GasEngine implements Engine {
+    
+    public static final String SERVICE_NAME = "GasEngine";
+    
+    @Override
+    public void spinWheels() {
+      // no-op for now
+    }
+
+  }
+  
+public static class ElectricEngine implements Engine {
+  public static final String SERVICE_NAME = "ElectricEngine";
+  
+    @Override
+    public void spinWheels() {
+      // no-op for now
+    }
+
+  }
+  
+  public static interface Engine {
+    void spinWheels();
+  }
+  
 }
