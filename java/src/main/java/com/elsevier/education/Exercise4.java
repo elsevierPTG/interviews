d2dbf85e5fab00d8fe3efe89198ae379aabb33d2
vv998 <vv998@mail.umkc.edu>
2017-04-25 19:12:57 -0400

    Interview coding examples

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,27 +1,32 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
+this is not a thread safe to make it thread safe we can use Atomic integer which
+is lock-free thread-safe 
+
 */
 public class Exercise4 {
 
-	public static class Counter {
-		
-		private int count = 0;
-		
-		public int increment() {
-			return ++count;
-		}
-		
-		public int getCount() {
-			return count;
-		}
-		
-		public void resetCount() {
-			count = 0;
-		}
+    public static class Counter {
+        
+        private AtomicInteger count = new AtomicInteger(0);
+        
+        public int increment() {
+            return count.incrementAndGet();
+        }
+        
+        public int getCount() {
+            return getCount();
+        }
+        
+        public void resetCount() {
+            count.set(0);
+        }
 
-	}
+    }
 }
--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -4,17 +4,27 @@ package com.elsevier.education;
 
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
 
+to make this class singleton making constructor private and getInstance method which is lazy loaded when needed
 */
 public class Exercise5 {
-	
-	public static class Singleton {
-		public void doSomething() {
-			System.out.println("Doing something....");
-	    }
-	}
-		
-	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
-	}
+    
+    public static class Singleton {
+        
+        private static Singleton instance = new Singleton();
+        
+        private Singleton() {
+        }
+        public static Singleton getInstance() {
+            return instance;
+        }
+
+        public void doSomething() {
+            System.out.println("Doing something....");
+        }
+    }
+        
+    public static void main(String a[]){
+        Singleton st = Singleton.getInstance();
+        st.doSomething();
+    }
 }
