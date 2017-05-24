843a468f30399a0926f06378b8f2bdd1b17e81da
borisfilipov <boris_filipov@yahoo.com>
2017-05-24 00:46:12 -0400

    Elsevier Interview assessment

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,9 +1,11 @@
 package com.elsevier.education;
 
 /**
-
-TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-
+* 1. Counter is not thread-safe, a single instance can be accessed by multiple threads.
+* 2. One approach to make methods thread-safe is to add keyword synchronized.
+* 3. synchronized keyword is not added to getCount() method to prevent unnecessary lock, because this method does not modify but only return
+* "count" field.
+* @author Boris Filipov
 */
 public class Exercise4 {
 
@@ -11,7 +13,7 @@ public class Exercise4 {
 		
 		private int count = 0;
 		
-		public int increment() {
+		public synchronized int increment() {
 			return ++count;
 		}
 		
@@ -19,7 +21,7 @@ public class Exercise4 {
 			return count;
 		}
 		
-		public void resetCount() {
+		public synchronized void resetCount() {
 			count = 0;
 		}
 
