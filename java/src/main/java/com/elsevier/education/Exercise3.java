ff56f3316c72472d8a97c955b2a5e01d40a8d224
kavithagampa5 <kavithagampa5@gmail.com>
2017-05-23 22:56:40 -0400

    Update Exercise3.java
     1. Random generator gives different values while calling hashCode method on same object or different object. 
     2. We do not need Random since we are trying to add same object twice, both objects should have similar hashCode value.
     3. We can use id as hashCode value.

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -7,13 +7,17 @@ import java.util.*;
 TODO Examine the failing test case for this class.
  We should be able to call people.add() twice but end with only one object in it.
  We can test with "gradlew test"
+ 
+ 1. Random generator gives different values while calling hashCode method on same object or different object. 
+ 2. We do not need Random since we are trying to add same object twice, both objects should have similar hashCode value.
+ 3. We can use id as hashCode value.
 
 */
 public class Exercise3 {
 
 	public static class Person {
 	
-		private static Random generator = new java.util.Random();
+		//private static Random generator = new java.util.Random();
 		private Integer id;
 		
 		public Person(int newId) {
@@ -21,7 +25,7 @@ public class Exercise3 {
 		}
 		
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id;// * generator.nextInt();
 		}
 		
 		public boolean equals(Object other) {
