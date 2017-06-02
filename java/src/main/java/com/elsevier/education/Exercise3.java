bd3766ce16835a8d6df9e9c933a6a87e8298cc2e
smchow <sunita.m.chowdhury@gmail.com>
2017-06-02 09:03:25 -0400

    solutions to duplicate key and immutabe class exercises

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -8,24 +8,27 @@ TODO Examine the failing test case for this class.
  We should be able to call people.add() twice but end with only one object in it.
  We can test with "gradlew test"
 
+ We can do this by ensuring that hashCode has the same key if the value is the same
+ We are generating a new key my using the Random number generator. If we remove this
+ then we are trying to add a second object with the same key and it will not add the
+ object the second time
 */
 public class Exercise3 {
 
-	public static class Person {
-	
-		private static Random generator = new java.util.Random();
-		private Integer id;
-		
-		public Person(int newId) {
-			id = newId;
-		}
-		
-		public int hashCode() {
-			return id * generator.nextInt();
-		}
-		
-		public boolean equals(Object other) {
-			return id.equals(((Person)other).id);
-		}
-	}
+    public static class Person {
+        
+        private Integer id;
+
+        public Person(int newId) {
+            id = newId;
+        }
+
+        public int hashCode() {
+            return id ;
+        }
+
+        public boolean equals(Object other) {
+            return id.equals(((Person)other).id);
+        }
+    }
 }
