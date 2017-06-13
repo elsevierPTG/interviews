3eb8dc7ed1f594a22bde37bb6d57ad62559cb214
interview-test <test-interview@mail.com>
2017-06-12 21:19:23 -0400

    make class immutable (Exercise 1)

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -2,6 +2,8 @@ package com.elsevier.education;
 
 import java.util.Set;
 
+import static java.util.Collections.unmodifiableSet;
+
 /**
 
 TODO: Make this class immutable.
@@ -10,33 +12,29 @@ TODO: Make this class immutable.
 public class Exercise1 {
 
 	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
+
+
+		public Person(Set<String> phoneNumbers, String firstName, String lastName) {
+			// use unmodifiable set in order to avoid changing phoneNumber outside this class
+			this.phoneNumbers = unmodifiableSet(phoneNumbers);
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
+
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
+
 		public String getLastName() {
 			return lastName;
-		}
-		public void setLastName(String newName) {
-			lastName = newName;
 		}
 	}
 }
