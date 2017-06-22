738b71ff0315e600e4127183ab0e7bfff1aa819f
rakesh0dp <rakesh.dp@hotmail.com>
2017-06-22 14:32:41 -0400

    Update Exercise5.java

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,20 +1,24 @@
-package com.elsevier.education;
-
-/**
-
-TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
-
-*/
-public class Exercise5 {
-	
-	public static class Singleton {
-		public void doSomething() {
-			System.out.println("Doing something....");
-	    }
-	}
-		
-	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
-	}
-}
+package com.elsevier.education;   
+/**    
+TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().   
+*/ 
+public class Exercise5 {      
+	public static class Singleton { 
+  		Singleton st = null;
+  		private Singleton() {
+  		}     
+		public static Singleton getSingleton() {   
+			if(st == null) {       
+				st = new Singleton();   
+			}   
+			return st;   
+  		}
+   		public void doSomething() {     
+			System.out.println("Doing something....");        
+		}   
+	}       
+	public static void main(String a[]) {    
+		Singleton st = Singleton.getSingleton();   
+		st.doSomeThing();   
+	} 
+} 
