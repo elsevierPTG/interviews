15166f8e42d109013b6fc49f53277499f050b822
Seth Fulmer <seth.fulmer@gmail.com>
2017-07-10 16:35:32 -0400

    Made appropriate modifications and finished exam

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -8,24 +8,32 @@ TODO Examine the failing test case for this class.
  We should be able to call people.add() twice but end with only one object in it.
  We can test with "gradlew test"
 
-*/
-public class Exercise3 {
+Note:
 
-	public static class Person {
-	
-		private static Random generator = new java.util.Random();
+Since I can't run gradle on my machine - I have no clue nor do I have gradle 
+installed on my machine, I copied the code to another project to test it and 
+removed the Random number generator and reference thereto.  I just return the id
+in the hashCode so it'll always return the same number for the same object multiple times.
+*/
+public class Exercise3
+{
+	public static class Person
+	{
 		private Integer id;
 		
-		public Person(int newId) {
+		public Person(int newId)
+		{
 			id = newId;
 		}
 		
-		public int hashCode() {
-			return id * generator.nextInt();
+		public int hashCode()
+		{
+			return(id);
 		}
 		
-		public boolean equals(Object other) {
-			return id.equals(((Person)other).id);
+		public boolean equals(Object other)
+		{
+			return(id.equals(((Person)(other)).id));
 		}
 	}
 }
