1061e3fbfed9a5d56b5b00dea53153a407c72754
Rakesh Patel <rakesh.dp@hotmail.com>
2017-07-10 16:07:38 -0400

    Add files via upload

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -6,28 +6,28 @@ import java.util.Set;
 
 TODO: Make this class immutable.
 
-Declared Exercise1 class as final and person class as finla
+Declared Exercise1 class as final and person class as final
 
 */
-public class Exercise1 {
+public final class Exercise1 {
 
 	public static class Person {
 		
-		final private Set<String> phoneNumbers;    
-		final private String firstName;    
-		final private String lastName;      
+		final private Set<String> phoneNumbers;
+		final private String firstName;
+		final private String lastName;
 		
-		public Person(Set ph, String fn, String ln) {    
-			phoneNumbers = ph;   
-			firstName = fn;   
-			lastName = ln;    
+		public Person(Set ph, String fn, String ln) {
+			phoneNumbers = ph;
+			firstName = fn;
+			lastName = ln;
 		} 
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
 		/*
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		public void setPhoneNumbers(Set<String> 				newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
 		}
 		*/
