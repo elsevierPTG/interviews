ce7bf72c1ae33b4315a626a25b92be197753ffcd
harshi16 <harshi.d2616@gmail.com>
2017-07-14 10:42:48 -0400

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,42 +1,35 @@
 package com.elsevier.education;
 
 import java.util.Set;
+	/** The Purpose of this class is to Make this class immutable.
+	 * The instance variable of the class is final i.e. we cannot change the value of it after creating an object.
+	 * The class is final so we cannot create the subclass.
+	 * There is no setter methods i.e. we have no option to change the value of the instance variable.
+	*/
+	
+		public final class Person{
+			
+			private final Set<String> phoneNumbers;
+			private final String firstName;
+			private final String lastName;
+			
+			
+			public Person(Set<String> newPhoneNumbers,String newFirstName,String newLastName) {
+				phoneNumbers = newPhoneNumbers;
+				firstName = newFirstName;
+				lastName = newLastName;
+			}
 
-/**
-
-TODO: Make this class immutable.
-
-*/
-public class Exercise1 {
-
-	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+			public Set<String> getPhoneNumbers() {
+				return phoneNumbers;
+			}
+			
+			public String getFirstName() {
+				return firstName;
+			}
+			
+			public String getLastName() {
+				return lastName;
+			}
+			
 		}
-
-		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
-		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
-		public String getFirstName() {
-			return firstName;
-		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
-		public String getLastName() {
-			return lastName;
-		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
-	}
-}
