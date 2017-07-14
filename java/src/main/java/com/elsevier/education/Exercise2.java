599a7faf388476778a5a3d27d02e0a5ffdc6fc70
harshi16 <harshi.d2616@gmail.com>
2017-07-14 10:44:43 -0400

    Update Exercise2.java

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,29 +1,40 @@
 package com.elsevier.education;
 
 /**
-
 TODO refactor the Car to use dependency injection of the engine
 TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
 TODO make sure we have no-op implementations of the gas engine and electric engine
-
+     The Purpose of this class is to use dependency injection using class as Abstract Class or by using Interface 
 */
 public class Exercise2 {
-
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
 	
-	public static class GasEngine {
+	public interface Car{
+		void moveForward();
+	}
+
+	public class GasEngine implements Car{
+		private GasEngine engine = new GasEngine();
+		@Override
+		public void moveForward() {
+			// TODO Auto-generated method stub
+			engine.spinWheels();
+			
+		}
+		
 		public void spinWheels() {
 			// no-op for now
 		}
+		
+	}
+
+	public class ElectricEngine implements Car{
+
+		@Override
+		public void moveForward() {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
+
