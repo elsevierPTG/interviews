993d38d7d3298f1d2780b1eaeb18ba9d193c2de1
Spindler, Justin <Justin.Spindler@inin.com>
2017-07-21 16:54:45 -0400

    Answers to tech questions

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,42 +1,44 @@
 package com.elsevier.education;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
 
 TODO: Make this class immutable.
 
+ 1. Modified the fields to be final.  Not wholly necessary but serves to prevent accidental mutation from within the class
+ 2. Removed mutable accessor methods.
+ 3. Added constructor which accepts the field values
+ 4. Copied the contents of the provided Set into a new HashSet so that changes to the original set would not be reflected
+ 5. Used Collections.unmodifiableSet to wrap the new HashSet in an immutable wrapper so that consumers of getPhoneNumbers
+    could not modify the contents.
 */
 public class Exercise1 {
 
 	public static class Person {
+
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+		public Person(String firstName, String lastName, Set<String> phoneNumbers) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.phoneNumbers = Collections.unmodifiableSet(new HashSet<String>(phoneNumbers));
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
 		
 		public String getLastName() {
 			return lastName;
-		}
-		public void setLastName(String newName) {
-			lastName = newName;
 		}
 	}
 }
--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -4,6 +4,17 @@ package com.elsevier.education;
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
+ No, Counter is not thread safe as it is possible for more than one thread to invoke each of the methods at the same time
+ and the mutation of the field is not atomic.  E.g. Thread #1 could read count at the same time Thread #2 reads count and then
+ both would attempt to write the same new value for count.
+
+ To fix this you could mark increment() and resetCount() as synchronized, or you could use AtomicInteger instead of int.
+
+ Despite that consumers of this thread could still result in thread-unsafe code if they depend on the result of getCount()
+ before invoking either increment() or resetCount().  Depending on the purpose of this counter both methods could be modified
+ to accept the expected current value and not mutate unless it matched, either within the synchronized block
+ or via AtomicInteger#compareAndSet
+
 */
 public class Exercise4 {
 
