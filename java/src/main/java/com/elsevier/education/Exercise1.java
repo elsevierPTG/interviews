a140eeafb1d108f1c61c8a10942985c45e5d228a
suhasp1j <suhas_jambhekar@yahoo.com>
2017-07-21 21:05:12 -0400

    Add files via upload
    Added changes by Suhas Jambhekar for nitin.malik@spectraforce.com

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -4,34 +4,30 @@ import java.util.Set;
 
 /**
 
-Make this class immutable.
-
+TODO: Make this class immutable.
  Steps:
- 	1) remove field Setters (commented here for past reference)
-    2) provide a constructor for setting
-    3) remove default constructor
-    4) make class final
+ 1) remove field Setters (commented here for past reference)
+ 2) provide a constructor for setting
+ 3) remove default constructor
+ 4) make class final
+
 */
-public final class Exercise1 {
+public class Exercise1 {
 
 	public static class Person {
-		
+
 		private final Set<String> phoneNumbers;
 		private final String firstName;
 		private final String lastName;
 
-        //Remove default constructor to abe able to make fields final, and get one chance to initialize using the
-        // following constructor
-//		public Person() {
-//		}
-
-        /**
-         * Constructor performing Shallow Copy
-         * @param firstName
-         * @param lastName
-         * @param phoneNumbers
-         */
-		public Person(String firstName,String lastName,Set<String> phoneNumbers) {
+		/**
+		 * Constructor performing Shallow Copy
+		 *
+		 * @param firstName
+		 * @param lastName
+		 * @param phoneNumbers
+		 */
+		public Person(String firstName, String lastName, Set<String> phoneNumbers) {
 			this.firstName = firstName;
 			this.lastName = lastName;
 			this.phoneNumbers = phoneNumbers;
@@ -41,22 +37,13 @@ public final class Exercise1 {
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-//		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-//			phoneNumbers = newPhoneNumbers;
-//		}
-		
+
 		public String getFirstName() {
 			return firstName;
 		}
-//		public void setFirstName(String newName) {
-//			firstName = newName;
-//		}
-//
+
 		public String getLastName() {
 			return lastName;
 		}
-//		public void setLastName(String newName) {
-//			lastName = newName;
-//		}
 	}
 }
--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -3,38 +3,45 @@ package com.elsevier.education;
 /**
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-	 Ans: No the class Counter is not thread-safe, since while one thread could be updating state, by changing int count
-	 value by invoking increment or resetCount, another thread could be reading count using getCount()
- 	The class Counter can be made thread-safe by
- 	1) making the class immutable, by making class final, removing increment and resetCount methods, making
- 		private final int count; and adding a constructor
-			 public Counter(int count) {
-			 	this.count = count;
-			 }
- 	2) Make field count volatile:  private volatile int count = 0;
- 	3) make the three methods synchronized as below
-		 public synchronized int increment() { return ++count; }
-		 public synchronized int getCount() { return count; }
-		 public synchronized void resetCount() { count = 0; }
+ Answer: No the class Counter is not thread-safe, since while one thread could be updating state, by changing int count
+ value by invoking increment or resetCount, another thread could be reading count using getCount()
+ The class Counter can be made thread-safe by
+ 1) making the class immutable, by making class final, removing increment and resetCount methods, making
+ private final int count; and adding a constructor
+ public Counter(int count) {
+ this.count = count;
+ }
+ 2) Make field count volatile:  private volatile int count = 0;
+ 3) make the three methods synchronized as below
+ public synchronized int increment() { return ++count; }
+ public synchronized int getCount() { return count; }
+ public synchronized void resetCount() { count = 0; }
 
- */
+*/
 public class Exercise4 {
 
-	public static class Counter {
+	public static final class Counter {
 		
-		private int count = 0;
-		
-		public int increment() {
-			return ++count;
+//		private int count = 0;
+//
+//		public int increment() {
+//			return ++count;
+//		}
+//
+//		public int getCount() {
+//			return count;
+//		}
+//
+//		public void resetCount() {
+//			count = 0;
+//		}
+		//With the first approach:
+		private final int count;
+		public Counter(int count) {
+			this.count = count;
 		}
-		
-		public int getCount() {
-			return count;
-		}
-		
-		public void resetCount() {
-			count = 0;
-		}
+
+		public  int getCount() { return count; }
 
 	}
 }
