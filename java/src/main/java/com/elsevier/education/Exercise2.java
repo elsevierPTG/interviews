77502b86cd5e97a1f6221d4c21ec31b4de0b3c40
Milton Quranda <milton.quranda@gmail.com>
2017-08-01 12:20:57 -0400

    Name: Milton Quranda email address: milton.quranda@gmail.com Recruider: NTTData Contact Person: Nicholas Croce recruiters email: nicholas.croce@nttdata.com

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -7,13 +7,18 @@ TODO allow use of either a gas engine or electric engine (create an appropriate 
 TODO make sure we have no-op implementations of the gas engine and electric engine
 
 */
+
+/**
+ * If new engine type is added, Engine can be abstracted to that sub engine type
+ */
 public class Exercise2 {
 
 	public static class Car {
+
+		private Engine engine;
 		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
+		public Car(Engine engine) {
+			this.engine = engine;
 		}
 		
 		public void moveForward() {
@@ -21,9 +26,27 @@ public class Exercise2 {
 		}
 	}
 	
-	public static class GasEngine {
+	public static class GasEngine extends Engine {
 		public void spinWheels() {
-			// no-op for now
+			System.out.println("This is a Gas engine");
 		}
+	}
+
+	public static class ElectricEngine extends Engine {
+		public void spinWheels() {
+			System.out.println("This is a Electric engine");
+		}
+	}
+
+	public static abstract class Engine {
+		public abstract void spinWheels();
+	}
+
+	public static void main(String[] args) {
+		Car electricCar = new Car(new ElectricEngine());
+		electricCar.engine.spinWheels();
+
+		Car gasCar = new Car(new GasEngine());
+		gasCar.engine.spinWheels();
 	}
 }
--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -7,23 +7,27 @@ import java.util.*;
 TODO Examine the failing test case for this class.
  We should be able to call people.add() twice but end with only one object in it.
  We can test with "gradlew test"
-
 */
+
+/**
+ * Changed the hashCode to return id hashcode rather then randomly generated value.
+ * This makes sure the hashcode of the instance is the same based on the provided integer
+ */
 public class Exercise3 {
 
 	public static class Person {
-	
+
 		private static Random generator = new java.util.Random();
 		private Integer id;
 		
 		public Person(int newId) {
 			id = newId;
 		}
-		
+
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id != null ? id.hashCode() : 0;
 		}
-		
+
 		public boolean equals(Object other) {
 			return id.equals(((Person)other).id);
 		}
