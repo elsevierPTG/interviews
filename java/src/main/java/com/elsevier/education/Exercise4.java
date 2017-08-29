e99b3f7a8ff7d4d5f7f7eb28cbfeefe48e70a9fe
Seth Fulmer <seth.fulmer@gmail.com>
2017-08-29 16:21:18 -0400

    Made changes as requested

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -4,24 +4,31 @@ package com.elsevier.education;
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
-*/
-public class Exercise4 {
+The only way I know to make this more thread safe is to synchronize the
+setters of the class, increment and resetCount so multiple threads 
+accessing it won't be able to screw up each other's counters.  I know 
+of nothing else to do to improve it.
 
-	public static class Counter {
-		
+*/
+public class Exercise4
+{
+	public static class Counter
+	{
 		private int count = 0;
 		
-		public int increment() {
-			return ++count;
+		synchronized public int increment()
+		{
+			return(++count);
 		}
 		
-		public int getCount() {
-			return count;
+		public int getCount()
+		{
+			return(count);
 		}
 		
-		public void resetCount() {
+		synchronized public void resetCount()
+		{
 			count = 0;
 		}
-
 	}
 }
