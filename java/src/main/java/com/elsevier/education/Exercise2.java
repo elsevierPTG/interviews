d2e1d0c80aa0cf158f1f9a7f8366e6e77b06ae77
shantis <smtshanti@gmail.com>
2017-08-31 23:19:56 -0700

    Completing the assessment questions

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,29 +1,65 @@
 package com.elsevier.education;
 
 /**
+ * TODO refactor the Car to use dependency injection of the engine TODO allow
+ * use of either a gas engine or electric engine (create an appropriate
+ * abstraction) TODO make sure we have no-op implementations of the gas engine
+ * and electric engine
+ */
 
-TODO refactor the Car to use dependency injection of the engine
-TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
-TODO make sure we have no-op implementations of the gas engine and electric engine
-
-*/
 public class Exercise2 {
+    /*
+     * Car class can have any kind of Engine (Gas or electric in this example)
+     * as long as it implements an interface called Engine.
+     */
+    public static class Car {
 
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
+	/*
+	 * 1. private variable to hold the type of Engine.
+	 *
+	 * 2. Dependency injection
+	 */
+	private Engine engine;
+
+	/*
+	 * Modified constructor to take the type of Engine
+	 */
+	public Car(Engine engine) {
+	    this.engine = engine;
 	}
-	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
+
+	public void moveForward() {
+	    this.engine.spinWheels();
 	}
+    }
+
+    public static class ElectricEngine implements Engine {
+	@Override
+	public void spinWheels() {
+	    // no-op for now
+	    // System.out.println("Electric engine");
+	}
+    }
+
+    public static interface Engine {
+	public void spinWheels();
+    }
+
+    public static class GasEngine implements Engine {
+	@Override
+	public void spinWheels() {
+	    // no-op for now
+	    // System.out.println("Gas engine");
+	}
+    }
+
+    public static void main(String[] args) {
+
+	Engine gasEngine = new GasEngine();
+	Car gasCar = new Car(gasEngine);
+	gasCar.moveForward();
+	Engine electricEngine = new ElectricEngine();
+	Car electricCar = new Car(electricEngine);
+	electricCar.moveForward();
+    }
 }
--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -1,31 +1,40 @@
 package com.elsevier.education;
 
-import java.util.*;
+import java.util.Random;
 
 /**
-
-TODO Examine the failing test case for this class.
- We should be able to call people.add() twice but end with only one object in it.
- We can test with "gradlew test"
-
-*/
+ * TODO Examine the failing test case for this class. We should be able to call
+ * people.add() twice but end with only one object in it. We can test with
+ * "gradlew test"
+ */
 public class Exercise3 {
 
-	public static class Person {
-	
-		private static Random generator = new java.util.Random();
-		private Integer id;
-		
-		public Person(int newId) {
-			id = newId;
-		}
-		
-		public int hashCode() {
-			return id * generator.nextInt();
-		}
-		
-		public boolean equals(Object other) {
-			return id.equals(((Person)other).id);
-		}
+    public static class Person {
+	// The random generator is going to generate a different id every time
+	// fooling the hashset that the id is different every time.
+	private static Random generator = new java.util.Random();
+	private Integer id;
+
+	public Person(int newId) {
+	    this.id = newId;
 	}
+
+	@Override
+	public boolean equals(Object other) {
+	    return this.id.equals(((Person) other).id);
+	}
+
+	@Override
+	public int hashCode() {
+	    /*
+	     * The hashCode() implementation violates the contract of hashCode.
+	     * A hashCode() method must return the same value each time it's
+	     * called for an object, provided that no information used in equals
+	     * comparisons on the object is modified. By returning the same id
+	     * value per object, there will only be one such object added.
+	     *
+	     */
+	    return this.id;
+	}
+    }
 }
