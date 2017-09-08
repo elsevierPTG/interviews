4efd045b12c48554fbb252dd7c5da974b106cbe5
Ashrithasammeta <asritha.sammeta@gmail.com>
2017-09-08 13:15:42 -0500

    Update Exercise3.java

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -2,13 +2,6 @@ package com.elsevier.education;
 
 import java.util.*;
 
-/**
-
-TODO Examine the failing test case for this class.
- We should be able to call people.add() twice but end with only one object in it.
- We can test with "gradlew test"
-
-*/
 public class Exercise3 {
 
 	public static class Person {
@@ -21,7 +14,7 @@ public class Exercise3 {
 		}
 		
 		public int hashCode() {
-			return id * generator.nextInt();
+			return id;
 		}
 		
 		public boolean equals(Object other) {
