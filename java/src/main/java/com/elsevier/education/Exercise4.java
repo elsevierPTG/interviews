90609aa3a47a6b70b5aec17bfdc43c7213e5be92
amarraja123 <amar.arawat@gmail.com>
2017-09-12 22:08:17 -0400

    name: Amarendra Kumar email address: amar.arawat@gmail.com recruiter's name: Collabera Inc.

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,26 +1,40 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
 
-TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-
-*/
+ TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+ <p>
+ No the Counter was not thread safe.
+ Why?: This is because the two threads can make the call to increment and getCount and could access counter object at once.
+ We can make the count Atomic so that the operations are atomic or was can create a synchronized block.
+ </p>
+ */
 public class Exercise4 {
 
 	public static class Counter {
-		
-		private int count = 0;
-		
+
+		private AtomicInteger count;
+
+		public Counter() {
+			this.count = new AtomicInteger();
+		}
+
+		public Counter(final int count) {
+			this.count = new AtomicInteger(count);
+		}
+
 		public int increment() {
-			return ++count;
+			return this.count.incrementAndGet();
 		}
-		
+
 		public int getCount() {
-			return count;
+			return this.count.get();
 		}
-		
+
 		public void resetCount() {
-			count = 0;
+			this.count.set(0);
 		}
 
 	}
