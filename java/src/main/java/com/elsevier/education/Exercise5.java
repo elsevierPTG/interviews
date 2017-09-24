8cd45e267fd8a427c79cb088020cbae33c02d294
darren.hall <dhall.public@gmail.com>
2017-09-24 10:29:42 -0400

    Completed commit

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -6,15 +6,45 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 
 */
 public class Exercise5 {
-	
+
+	/**
+	 * Singleton class.
+	 */
 	public static class Singleton {
+		// class member varaibles
+		private static final Singleton singleton = new Singleton();
+
+		/**
+		 * Private constructor to encourage consumers to use the getSingleton() method to
+		 * obtain a reference to the Singleton
+		 */
+		private Singleton() {}
+
+		/**
+		 * The approved way to get a reference to the Singleton object
+		 *
+		 * @return
+		 *      Singleton, a reference to the only instance of itself
+		 */
+		public static Singleton getSingleton() {
+			return singleton;
+		}
+
+		/**
+		 * Does something
+		 */
 		public void doSomething() {
 			System.out.println("Doing something....");
-	    }
+		}
 	}
-		
+
+	/*
+	 * Appliction main method
+	 */
 	public static void main(String a[]){
-		Singleton st = new Singleton();
-		st.doSomeThing();
+		// get a reference to the Singleton
+		Singleton st = Singleton.getSingleton();
+		// do something
+		st.doSomething();
 	}
 }
