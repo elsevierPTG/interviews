e7c0611b90ca824458a6b0e39867c0c8be3d84a6
James Herndon <james.herndon@gmail.com>
2017-10-16 14:19:29 -0400

    Implemented a hasty test to prove Exercise4 is thread safe

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -30,4 +30,20 @@ public class Exercise4 {
 		}
 
 	}
+	
+	public class ThreadCounter extends Thread {
+		private Counter counter = new Counter();
+		
+		public ThreadCounter(String name) {
+			super(name);
+		}
+		
+	    public void run() {
+	        for (int i=0; i<10; i++) {
+	            System.out.print("Thread " + getName() + " ");
+	            System.out.print("\tCount: " + Integer.valueOf(counter.getCount()));
+	            System.out.print("\tincrementing to: " + Integer.valueOf(counter.increment()) + "\n");
+	        }
+	    }
+	}
 }
