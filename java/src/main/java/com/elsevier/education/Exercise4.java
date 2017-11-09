7ae82a3d1027ae7f060339e7e28f1c2aecda4ce4
Matthew Morrissey <mattamizer@gmail.com>
2017-11-08 21:12:12 -0500

    Exercise Four solution: Make the class thread safe
    This commit adds `synchronize` to the increment method implementation to
    ensure that it is thread safe.`

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -4,21 +4,26 @@ package com.elsevier.education;
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
+Answer: No, it is not currently threadsafe.
+The initial implementaion has a race condition whereby someone could be accessing the getter while the increment is
+being run. This would result in undesiralbe output. See below for the fix.
+
 */
 public class Exercise4 {
 
 	public static class Counter {
-		
+
 		private int count = 0;
-		
-		public int increment() {
+
+        // Make sure we lock this down when incrementing so that we don't try to access the variable in flight
+		public synchronized int increment() {
 			return ++count;
 		}
-		
+
 		public int getCount() {
 			return count;
 		}
-		
+
 		public void resetCount() {
 			count = 0;
 		}
