4d22a5bd4f8cd94fc623c146a88c644fd977ad88
Jeff Zheng <Jeff Zheng@192.168.0.25>
2017-12-12 12:57:22 -0500

    code challenge

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -11,9 +11,10 @@ public class Exercise2 {
 
 	public static class Car {
 		
-		private GasEngine engine = new GasEngine();
+		private Engine engine;
 		
-		public Car() {
+		public Car(Engine engine) {
+			this.engine = engine;
 		}
 		
 		public void moveForward() {
@@ -21,7 +22,19 @@ public class Exercise2 {
 		}
 	}
 	
-	public static class GasEngine {
+	public static interface Engine {
+		public void spinWheels();
+	}
+	
+	public static class GasEngine implements Engine {
+		@Override
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
+	
+	public static class ElectricEngine implements Engine {
+		@Override
 		public void spinWheels() {
 			// no-op for now
 		}
