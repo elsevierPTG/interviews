aee19a1a58a2801435cc9cb7bfef3afccf2d525a
Rodin, Elena (Contractor) <Elena_Rodin@comcast.com>
2017-12-14 22:49:21 -0500

    completed E1

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,5 +1,7 @@
 package com.elsevier.education;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,36 +9,73 @@ import java.util.Set;
 TODO: Make this class immutable.
 
 */
-public class Exercise1 {
 
-	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
+ public final class Exercise1 {
+
+
+	public class Person {
+
+		/**
+		 * Person is an immutable class, since there is no way to change
+		 * its state after construction.
+		 */
+
+		public Person(String aFirstName, String aLastName, Set<String> aPhoneNumbers) {
+		    firstName = aFirstName;
+		    lastName = aLastName;
+		    phoneNumbers = new HashSet<>(Arrays.asList("2155555555", "2157777777"));
 		}
 
-		public Set<String> getPhoneNumbers() {
+        /**
+         * An immutable object fields. (String objects never change state.)
+         */
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
+
+        /**
+         * Returns an immutable object.
+         *
+         * The caller gets a direct reference to the internal field. But this is not
+         * dangerous, since String is immutable and cannot be changed.
+         */
+        public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
-		public String getFirstName() {
+
+		//Dont allow mutators (setters) for the attributes
+//		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+//			phoneNumbers = newPhoneNumbers;
+//		}
+
+        /**
+         * Returns an immutable object.
+         *
+         * The caller gets a direct reference to the internal field. But this is not
+         * dangerous, since String is immutable and cannot be changed.
+         */
+        public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
-		public String getLastName() {
+
+        //Dont allow mutators (setters) for the attributes
+//        public void setFirstName(String newName) {
+//			firstName = newName;
+//		}
+
+        /**
+         * Returns an immutable object.
+         *
+         * The caller gets a direct reference to the internal field. But this is not
+         * dangerous, since String is immutable and cannot be changed.
+         */
+        public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+
+        //Dont allow mutators (setters) for the attributes
+//        public void setLastName(String newName) {
+//			lastName = newName;
+//		}
 	}
 }
