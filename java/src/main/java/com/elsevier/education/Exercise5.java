16eccd751ccb16ef556beaf7469db5d989bc3515
nateddd <35239481+nateddd@users.noreply.github.com>
2018-01-08 17:40:35 -0500

    Update Exercise5.java
    added code

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -8,6 +8,24 @@ TODO: Turn the "Singleton" class into an actual singleton. The main() method sho
 public class Exercise5 {
 	
 	public static class Singleton {
+		
+		 private static Singleton st;
+    /**
+     * Create private constructor
+     */
+    private Singleton(){
+         
+    }
+     /**
+     * Create a static method to get instance.
+     */
+    public static Singleton getInstance(){
+        if(st == null){
+            st = new Singleton();
+        }
+        return myObj;
+    }
+		
 		public void doSomething() {
 			System.out.println("Doing something....");
 	    }
