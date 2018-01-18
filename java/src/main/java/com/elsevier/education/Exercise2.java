72c717bea9bf7647bcf313c2c797238ad75db91b
vinayksrinivasan <vinayiyengar@gmail.com>
2018-01-18 14:08:47 -0500

    added exercises 1 thru 5 with the functionality in the TODO section

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -6,24 +6,52 @@ TODO refactor the Car to use dependency injection of the engine
 TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
 TODO make sure we have no-op implementations of the gas engine and electric engine
 
+   Created the Engine interface for abstraction and two implementations EngineGas and EngineElectric.
+   Dependency Injection via constructor, resulting in two instances of the Car class. 
+
 */
 public class Exercise2 {
 
 	public static class Car {
+		private Engine engine;
 		
-		private GasEngine engine = new GasEngine();
+		public Car(Engine engine) {
+			this.engine=engine;
+		}
+
+		public Engine getEngine() {
+			return engine;
+		}
+		/*private GasEngine engine = new GasEngine();
 		
 		public Car() {
-		}
+		}*/
 		
 		public void moveForward() {
 			engine.spinWheels();
 		}
-	}
+		
+		public interface Engine {
+			void spinWheels();
+		}
+		
+		public static class EngineGas implements Engine {
+			@Override
+			public void spinWheels() {
+				System.out.println("****In gas engine****");
+			}
+		}
 	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
+		public static class EngineElectric implements Engine {
+			@Override
+			public void spinWheels() {
+				System.out.println("***In electric engine***");
+			}
+		}
+	
+		public static void main(String... args) {
+			new Car(new EngineGas()).moveForward();
+			new Car(new EngineElectric()).moveForward();
 		}
 	}
 }
