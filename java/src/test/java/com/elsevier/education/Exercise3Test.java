937c81815ac0978e27c899dc65f1e5ad1aabe32a
Thomas Apicella <tom.apicella@thomsonreuters.com>
2018-01-21 17:53:47 -0500

    Exercise 3

--- java/src/test/java/com/elsevier/education/Exercise3Test.java
+++ java/src/test/java/com/elsevier/education/Exercise3Test.java
@@ -13,9 +13,16 @@ public class Exercise3Test {
         Set<Person> people = new HashSet<>();
 
         Person p1 = new Person(1);
+        Person p1b = new Person(1);
+        
         people.add(p1);
         people.add(p1);
+		assertEquals(1, people.size());
 
-        assertEquals(1, people.size());
+		//p1 and p1_b have the same ID, but different hashes, so they are "equal," but will both be added to the HashSet
+		people.add(p1b);
+		assertEquals(p1, p1b);
+		assertTrue(p1.hashCode() != p1b.hashCode());
+		assertEquals(2, people.size());
     }
 }
