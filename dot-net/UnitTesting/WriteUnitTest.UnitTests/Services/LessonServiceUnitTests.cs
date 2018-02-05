d31834071e12c4ea98a45acda15eac67d098e77c
adamwaynetowle <adam.w.towle@gmail.com>
2018-02-05 15:16:44 -0500

    Update LessonServiceUnitTests.cs

--- dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
+++ dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WriteUnitTest.Repositories;
+using WriteUnitTest.Services;
+
 
 namespace WriteUnitTest.UnitTests.Services
 {
     [TestClass]
     public class LessonServiceUnitTests
     {
-        [TestMethod]
-        public void UpdateLessonGrade_Test()
-        {
-        }
+          private static readonly int lessonId = 12;
+
+          [TestMethod]
+          public void UpdateLessonGrade_Test()
+          {
+               var moduleRepo = new ModuleRepository();
+               var lessonService = new LessonService();
+               var module = moduleRepo.GetModule(lessonId);
+
+               var grade = new Random().Next(0, 100);
+               var isPassing = module.MinimumPassingGrade <= grade;
+
+               try
+               {
+                    var updatedLesson = lessonService.UpdateLessonGrade(lessonId, grade);
+                    Assert.AreEqual(updatedLesson.Grade, grade);
+                    Assert.AreEqual(updatedLesson.IsPassed, isPassing);
+
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine(e);
+               }
+
+          }
     }
 }
