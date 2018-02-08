05a66e99460b9d4237c316a680b26ef423986bf5
mikeweltejr <mikewelte22@gmail.com>
2018-02-07 20:52:01 -0500

    Interview Test
    I wrote in what I would fix for the sample application, and wrote a unit test for the unit test app. I saw that we could refactor the application, but I did not actually refactor instead I wrote the unit test based on the following assumptions: 1. Each repository has an interface it implements so it can be easily mocked 2. The function returns a lesson when done so you can assert from that.

--- dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
+++ dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
@@ -8,6 +8,36 @@ namespace WriteUnitTest.UnitTests.Services
         [TestMethod]
         public void UpdateLessonGrade_Test()
         {
+            var lesson = new Lesson
+                {
+                    LessonId = 12,
+                    Grade = 95.4,
+                    IsPassed = true
+                };
+
+            var module = new Module
+                {
+                    ModuleId = 1,
+                    MinimumPassingGrade = 93,
+                    Lessons = new List<Lesson>
+                    {
+                        new Lesson
+                        {
+                            LessonId = 12,
+                            Grade = 9.4,
+                            IsPassed = false
+                        }
+                    }
+                };
+            var mockLessonRepo = new DynamicMock(typeof (ILessonRepository));
+            var mockModuleRepo = new DynamicMock(typeof (IModuleRepository));
+            mockLessonRepo.ExpectAndReturn("GetLesson", lesson);
+            mockModuleRepo.GetModule("GetModule", module);
+
+            var testLessonService = new LessonService();
+            var retLesson = testLessonService.UpdateLessonGrade(12, 100);
+            Assert.AreEqual(100, retLesson.Grade);
+            Assert.AreEqual(true, retLesson.IsPassed);
         }
     }
 }
