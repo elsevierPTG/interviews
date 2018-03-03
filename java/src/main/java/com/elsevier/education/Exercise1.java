d0875fb04eb83a8ee90aaddbbc6f198d1185d53f
synkarius <dconway1985@gmail.com>
2018-03-03 07:41:08 -0500

    complete exercises

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,42 +1,47 @@
 package com.elsevier.education;
 
+import java.util.Collections;
 import java.util.Set;
 
 /**
 
-TODO: Make this class immutable.
+I got rid of the setters instead of changing them such that they returned
+an immutable copy of the Person object with that one field changed since
+the instructions only say "Make this class immutable", but if that was the
+intention, those getters would look like this:
+
+		public Person setFirstName(String newFirstName) {
+			return new Person(phoneNumbers, newFirstName, lastName);
+		}
 
 */
 public class Exercise1 {
 
 	public static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		/** Guava's ImmutableSet::copyOf is truly immutable, but Guava 
+		 * isn't in build.gradle, so I went with the standard library here.
+		 */
+		public Person(Set<String> phoneNumbers, String firstName, String lastName) {
+			this.phoneNumbers = Collections.unmodifiableSet(phoneNumbers);
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
-
+		
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
-		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
 		}
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
 		
 		public String getLastName() {
 			return lastName;
-		}
-		public void setLastName(String newName) {
-			lastName = newName;
 		}
 	}
 }
--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,26 +1,29 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
 
-TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+It is not thread safe. If two threads access 'count', reading or updating it,
+they may get thread-local cached versions of it.
 
 */
 public class Exercise4 {
 
 	public static class Counter {
 		
-		private int count = 0;
+		private AtomicInteger count = new AtomicInteger();
 		
 		public int increment() {
-			return ++count;
+			return count.incrementAndGet();
 		}
 		
 		public int getCount() {
-			return count;
+			return count.get();
 		}
 		
 		public void resetCount() {
-			count = 0;
+			count.set(0);
 		}
 
 	}
