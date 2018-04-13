7f3cc86fc360784b9e84db078b5c74a888c1bc22
peterbh <peter.b.howells@gmail.com>
2018-04-13 08:31:15 -0400

    finished examples

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -5,38 +5,38 @@ import java.util.Set;
 /**
 
 TODO: Make this class immutable.
+ removed setters and made things final, was thinking of builder to make it easy instead of constructor
+ but figured it was good enough.
 
 */
 public class Exercise1 {
 
 	public static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		public Person(Set<String> phoneNumbers,
+					  String firstName,
+					  String lastName) {
+			this.phoneNumbers = phoneNumbers;
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
 		
 		public String getFirstName() {
 			return firstName;
-		}
-		public void setFirstName(String newName) {
-			firstName = newName;
 		}
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+
+
 	}
 }
