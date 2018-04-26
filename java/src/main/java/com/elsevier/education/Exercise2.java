cc94136e177e28befb045633a6920481d7c5ee05
John D. Ament <john.d.ament@gmail.com>
2018-04-26 19:30:09 -0400

    Interview Submission from John D. Ament Recruiter: Patrick Irwin, Matt Montague, Intelligent Capital Network

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -1,29 +1,34 @@
 package com.elsevier.education;
 
 /**
-
-TODO refactor the Car to use dependency injection of the engine
-TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
-TODO make sure we have no-op implementations of the gas engine and electric engine
-
-*/
+ * TODO refactor the Car to use dependency injection of the engine
+ * TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
+ * TODO make sure we have no-op implementations of the gas engine and electric engine
+ */
 public class Exercise2 {
 
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
-	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
+    public static class Car {
+
+        private final Engine engine;
+
+        public Car(Engine engine) {
+            this.engine = engine;
+        }
+
+        public void moveForward() {
+            engine.spinWheels();
+        }
+    }
+
+    interface Engine {
+        default void spinWheels() {
+            // since its no-op, using a default method
+        }
+    }
+
+    public static class GasEngine implements Engine {
+    }
+
+    public static class ElectricEngine implements Engine {
+    }
 }
--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -1,27 +1,75 @@
 package com.elsevier.education;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
-
-TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
-
-*/
+ * TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+ * <p>
+ * John: No, this class is not threadsafe.  Below is a few different implementations that are thread safe
+ */
 public class Exercise4 {
 
-	public static class Counter {
-		
-		private int count = 0;
-		
-		public int increment() {
-			return ++count;
-		}
-		
-		public int getCount() {
-			return count;
-		}
-		
-		public void resetCount() {
-			count = 0;
-		}
+    public static class Counter {
 
-	}
+        private int count = 0;
+
+        public int increment() {
+            return ++count;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void resetCount() {
+            count = 0;
+        }
+
+    }
+
+    /**
+     * This works, but is likely a bad approach.  It uses a locking object to ensure single access to the counter.
+     * The lock should be final, to ensure it cannot change.
+     */
+    public static class SynchronizedCounter {
+        private final Object lock = new Object();
+        private int count = 0;
+
+        public int increment() {
+            synchronized (lock) {
+                return ++count;
+            }
+        }
+
+        public int getCount() {
+            synchronized (lock) {
+                return count;
+            }
+        }
+
+        public void resetCount() {
+            synchronized (lock) {
+                count = 0;
+            }
+        }
+    }
+
+    /**
+     * Using an atomic reference ensures proper thread safety
+     */
+    public static class AtomicCounter {
+        private AtomicInteger count = new AtomicInteger(0);
+
+        public int increment() {
+            return count.incrementAndGet(); // use increment and get to perform the increment before returning the value
+        }
+
+        public int getCount() {
+            return count.intValue();
+        }
+
+        public void resetCount() {
+            count.set(0);
+        }
+    }
 }
