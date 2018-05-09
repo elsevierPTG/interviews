abd6ab1b9b0587a1d31142224d265b8d2e3ebb31
alalicon577 <arnel577@yahoo.com>
2018-05-09 14:05:56 -0400

    Update Exercise2.java

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -6,14 +6,19 @@ TODO refactor the Car to use dependency injection of the engine
 TODO allow use of either a gas engine or electric engine (create an appropriate abstraction)
 TODO make sure we have no-op implementations of the gas engine and electric engine
 
+@author ArnelLalicon
+	a. create Engine class
+	b. create ElectricEngine class
+	c. GasEngine and ElectricEngine extended Engine class and override no-op method of parent class Engine
 */
 public class Exercise2 {
 
 	public static class Car {
 		
-		private GasEngine engine = new GasEngine();
+		private Engine engine;
 		
-		public Car() {
+		public Car(Engine engine) {
+			this.engine = engine;
 		}
 		
 		public void moveForward() {
@@ -21,7 +26,24 @@ public class Exercise2 {
 		}
 	}
 	
-	public static class GasEngine {
+	public static class Engine {
+		
+		public void spinWheels(){
+			
+		}
+	}
+	
+	public static class GasEngine extends Engine{
+		
+		@Override
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
+	
+	public static class ElectricEngine extends Engine{
+		
+		@Override
 		public void spinWheels() {
 			// no-op for now
 		}
