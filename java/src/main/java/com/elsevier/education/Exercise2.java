bd52c9ab94f76a78f45b21599dc120a49e8df7bb
Dmitry Kolesnikov <git-scm@8e4.net>
2018-06-09 21:15:55 -0400

    Dmitry Kolesnikov - Exercise Answers

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -7,21 +7,36 @@ TODO allow use of either a gas engine or electric engine (create an appropriate 
 TODO make sure we have no-op implementations of the gas engine and electric engine
 
 */
+
+/**
+ * Made use of Java inheritance here to create different types of engines
+ */
 public class Exercise2 {
 
 	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
+
+		private Engine engine;
+
+		public Car(Engine engine) {
+			this.engine = engine;
 		}
-		
+
 		public void moveForward() {
 			engine.spinWheels();
 		}
 	}
-	
-	public static class GasEngine {
+
+	public static abstract class Engine {
+		public abstract void spinWheels();
+	}
+
+	public static class GasEngine extends Engine {
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
+
+	public static class ElectricEngine extends Engine {
 		public void spinWheels() {
 			// no-op for now
 		}
