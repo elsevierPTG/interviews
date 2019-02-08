0962206e78931326ca796bbf05a03a1f7dde6bb6
Stephen Picone <sgpicone@gmail.com>
2019-02-08 15:25:32 -0500

    Adding unit tests to WriteUnitTest project

--- dot-net/UnitTesting/WriteUnitTest/Services/LessonService.cs
+++ dot-net/UnitTesting/WriteUnitTest/Services/LessonService.cs
@@ -1,24 +1,60 @@
-﻿using WriteUnitTest.Repositories;
+﻿using WriteUnitTest.Entities;
+using WriteUnitTest.Repositories;
 
 namespace WriteUnitTest.Services
 {
     public class LessonService
     {
+        // Create private fields for repositories to protect them from unwanted modification
+        private LessonRepository _LessonRepo;
+        private ModuleRepository _ModuleRepository;
+
+        
         public LessonService()
         {
+            /* 
+             * Instantiate repositories in LessonService constructor so that the instance isn't instantiating
+             * repositories on the fly and their state at the time of LessonService's creation is preserved.
+             */
+            LessonRepo = new LessonRepository();
+            ModuleRepository = new ModuleRepository();
         }
 
+        /* 
+         * Create public getters for our private fields; protect the setters from outside modification
+         * as it doesn't seem necessary to allow outside classes to modify this data.
+         */
+        public LessonRepository LessonRepo { get => _LessonRepo; protected set => _LessonRepo = value; }
+        public ModuleRepository ModuleRepository { get => _ModuleRepository; protected set => _ModuleRepository = value; }
+
+        /// <summary>
+        /// Update the grade of a lesson and evaluate whether that lesson is passed or not.
+        /// Checks the provided grade against the provided lesson's module data to verify
+        /// that the grade is greater than the module's minimum grade. If so, changes 
+        /// lesson's IsPassed field to true.
+        /// </summary>
+        /// <param name="lessonId">The ID of the lesson to be updated</param>
+        /// <param name="grade">The grade for the lesson</param>
         public void UpdateLessonGrade(int lessonId, double grade)
         {
-            var lessonRepo = new LessonRepository();
-            var lesson = lessonRepo.GetLesson(lessonId);
+            Lesson lesson = LessonRepo.GetLesson(lessonId);
 
+            //Throw an exception if no lesson is found and allow caller to handle the error.
+            if(lesson ==null)
+            {
+                throw new System.ArgumentOutOfRangeException("lessonId", lessonId, "Lesson id not found!");
+            }
+            
             lesson.Grade = grade;
 
             if (!lesson.IsPassed)
             {
-                var moduleRepository = new ModuleRepository();
-                var module = moduleRepository.GetModule(lessonId);
+                Module module = ModuleRepository.GetModule(lessonId);
+                //Throw an exception if no module is found and allow caller to handle the error.
+                if (module == null)
+                {
+                    throw new System.ArgumentOutOfRangeException("lessonId", lessonId, "No module found for lesson!");
+                }
 
                 if (grade >= module.MinimumPassingGrade)
                 {
