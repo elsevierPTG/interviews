06e537512a9832d1f05adce72f01778f7742a2ae
Susheel Bajaj <susheel.bajaj@ascensus.com>
2019-05-14 22:53:19 -0400

    refactored the code for Unit testing and added unit test cases along with design decision in INSTRUCTIONS.txt.
    Also added suggested changes for WhatWouldYouChange project

---
+++ dot-net/UnitTesting/WriteUnitTest/Repositories/ILessonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WriteUnitTest.Entities;
+
+namespace WriteUnitTest.Repositories
+{
+    public interface ILessonRepository
+    {
+        Lesson GetLesson(int lessonId);
+    }
+}
---
+++ dot-net/UnitTesting/WriteUnitTest/Services/ILessonService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteUnitTest.Services
+{
+    public interface ILessonService
+    {
+        void UpdateLessonGrade(int lessonId, double grade);
+    }
+}
