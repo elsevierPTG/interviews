b69870c184091098894015ea830101ee475e8bed
jreimMBP <jessereim@gmail.com>
2019-05-23 00:47:54 -0400

    submitting exercises

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -3,18 +3,31 @@ package com.elsevier.education;
 /**
 
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
+ //There is a really cool library out there called lombok https://projectlombok.org/ which uses an annotation for this
+ //by actually adding the byte code to the class file but not muddying up the java.
+ //I did assume that you wanted to see the implemtation tho.
 
 */
 public class Exercise5 {
-	
+
+
 	public static class Singleton {
-		public void doSomething() {
+
+		private static Singleton singleton;
+		private Singleton(){}
+		public static Singleton getInstance(){
+			if(singleton == null){
+				singleton = new Singleton();
+			}
+			return singleton;
+		}
+		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
 		
 	public static void main(String a[]){
-		Singleton st = new Singleton();
+		Singleton st = Singleton.getInstance();
 		st.doSomeThing();
 	}
 }
