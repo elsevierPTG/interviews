c7db4b20ce738562e2862c627ccc29b16a3eb7b5
Sagar Patel <sagarpatel@Sagars-MBP.fios-router.home>
2020-02-01 16:01:07 -0500

    Complete Test Assessment for Sagar Patel

--- backend/book-match/src/main/java/match/client/BookMatchClient.java
+++ backend/book-match/src/main/java/match/client/BookMatchClient.java
@@ -1,11 +1,29 @@
 package match.client;
 
+import match.dao.BookDao;
+import match.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public final class BookMatchClient {
+
+	@Autowired
+	private BookDao bookDao;
+
+	public BookMatchClient() {
+	}
+
+	public BookMatchClient(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
 
 	public static void main(String[] args) {
 		BookMatchClient bookMatchClient = startFramework();
@@ -21,15 +39,29 @@ public final class BookMatchClient {
 	 * Implement this method 
 	 */
 	public void matchBooks() {
-		
+		ArrayList<Book> bookArrayList = (ArrayList<Book>) bookDao.fetchBookList();
+
 		boolean bookMatches = false;
 		for (BooksToMatch bookToMatch : BooksToMatch.values()) {
-			
-			
-			//***  Implement the book matching logic
-//			bookMatches = ??? 
-			//***  
-			
+
+			String bookTitle = bookToMatch.getTitle();
+			Pattern stopWords = Pattern.compile("\\b(?:i|the|of|and|in)\\b\\s*", Pattern.CASE_INSENSITIVE);
+
+
+			bookTitle = bookTitle.replaceAll("\\p{Punct}","");
+
+			Matcher matcher = stopWords.matcher(bookTitle);
+			String cleanBookTitle = matcher.replaceAll("");
+
+			for (Book book : bookArrayList) {
+				if (book.getBookTitle().equalsIgnoreCase(cleanBookTitle)) {
+					bookMatches = true;
+					break;
+				}
+
+			}
+
+
 			System.out.println(bookToMatch.getTitle() + ": Exists ->" + bookMatches);
 		}
 		
--- backend/book-match/src/main/java/match/dao/BookDaoImpl.java
+++ backend/book-match/src/main/java/match/dao/BookDaoImpl.java
@@ -9,8 +9,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class BookDaoImpl implements BookDao {
 
 	@Autowired
