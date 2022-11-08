2eedb95de0685f98a2ed788ae31007a3303435d7
shamnitjsr <shamnitjsr@gmail.com>
2022-11-08 07:59:37 +0530

    PATIENT_API

--- java-spring-api/src/main/java/com/elsevier/javaspringapi/App.java
+++ java-spring-api/src/main/java/com/elsevier/javaspringapi/App.java
@@ -2,7 +2,11 @@ package com.elsevier.javaspringapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan("com.elsevier.javaspringapi.models")
+@EnableJpaRepositories("com.elsevier.javaspringapi.repository")
 @SpringBootApplication
 public class App {
 
